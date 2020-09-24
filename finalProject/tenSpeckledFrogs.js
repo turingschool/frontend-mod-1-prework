a3255f96c0efd2b8055f52d35b05208d65c96a08
Richard Tyler <richardtyler2011@gmail.com>
2020-09-23 21:05:39 -0600

    Make variables to clean up strings in code

--- finalProject/tenSpeckledFrogs.js
+++ finalProject/tenSpeckledFrogs.js
@@ -1,9 +1,15 @@
+var text = 'sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then '
+
+
+
+
+
 for (var i = 10; i > 0; i--) {
-  if (i > 2){
-  console.log(i + ' ' + 'speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were' + ' ' + (i - 1) + ' ' + 'speckled frogs.');
-} else if (i === 2) {
-  console.log(i + ' ' + 'speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was' + ' ' + (i - 1) + ' ' + 'speckled frog.');
-} else if (i === 1) {
-  console.log(i + ' ' + 'speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs.');
-}
-}
+    if (i > 2){
+    console.log(i + ' speckled frogs ' + text + 'there were ' + (i - 1) + ' speckled frogs.');
+  } else if (i === 2) {
+    console.log(i + ' speckled frogs ' + text + 'there was ' + (i - 1) + ' speckled frog.');
+  } else if (i === 1) {
+    console.log(i + ' speckled frog ' + text + 'there were no more speckled frogs.');
+  }
+};
