ab9e19bb7740bb041b0ef3357d0e3ad9c43bc064
Paige Vannelli <paigevannelli@gmail.com>
2020-10-15 15:06:59 -0600

    Initial commit

--- section2/exercises/exercises/comparisons.js
+++ section2/exercises/exercises/comparisons.js
@@ -5,7 +5,7 @@ to achieve the desired result.
 One example is already completed. Your task is to complete
 any remaining prompt.
 
-Make sure to run the file with node in your command line. Look back 
+Make sure to run the file with node in your command line. Look back
 at the directions from Section 1 if you need a refresher on how to do that.
 */
 
@@ -19,13 +19,15 @@ var stringTeachers = "4";
 var numberDogs = 0;
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
-console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents); 
+console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+console.log('Is numberTeachers less than numberStudents?', numberTeachers < numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
+console.log('Is numberTeachers equal to stringTeachers?', numberTeachers == stringTeachers);
 // this should log: true
 
 /*
@@ -35,6 +37,7 @@ perform this evaluation
 */
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
+console.log('Is numberTeachers strictly equal to stringTeachers?', numberTeachers === stringTeachers');
 // this should log: false
 
 /*
@@ -48,18 +51,23 @@ you develop good habits that follow best practice, from now on, use the strict c
 */
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log('Is numberTeachers not equal to numberStudents?', numberTeachers !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log('Is numberStudents greater than or equal to 20?', numberStudents >= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log('Is numberStudents greater than or equal to 21?', numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log('Is numberStudents less than or equal to 20?', numberStudents <= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log('Is numberStudents less than or equal to 21?', numberStudents <= 21);
 // this should log: true
 
 
@@ -67,9 +75,9 @@ you develop good habits that follow best practice, from now on, use the strict c
 // PART 2: Articulating what you are doing
 //-------------------
 
-// For the following prompts, you will be given a line of code and your task is to type out a Comment, 
-// in English, explaining what that line of code is doing, including what the comparison will evaluate to. 
-// Be as technically precise as possible, but don't just copy and paste a definition from the readings. 
+// For the following prompts, you will be given a line of code and your task is to type out a Comment,
+// in English, explaining what that line of code is doing, including what the comparison will evaluate to.
+// Be as technically precise as possible, but don't just copy and paste a definition from the readings.
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
@@ -127,5 +135,5 @@ var age = 1;
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-  // as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+  // as you would expect?
