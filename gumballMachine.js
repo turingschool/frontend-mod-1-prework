a2b5d6662805d0b7162b1fe2a654f570a44bb311
Julia Iwinski <juliaiwinski@gmail.com>
2020-10-14 19:58:19 -0700

    Gumball code

---
+++ gumballMachine.js
@@ -0,0 +1,11 @@
+
+
+function gumballMachine(money){
+  if (money >= 25){
+    console.log("here is a gumball");
+  }
+  else {
+    console.log("sorry not enough money");
+  }
+}
+gumballMachine(25)
