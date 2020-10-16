d0b97ba118321ab55bd1246a288b59cd7c54feba
Paige Vannelli <paigevannelli@gmail.com>
2020-10-15 21:53:48 -0600

    Add functions to the functions.js

--- section2/exercises/exercises/functions.js
+++ section2/exercises/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -27,6 +27,13 @@ printName();
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
 
+function sayHello() {
+  console.log('Oh, Hello!');
+}
+
+sayHello();
+sayHello();
+
 //-------------------
 // PART 2: Arguments and Parameters
 //-------------------
@@ -35,18 +42,34 @@ printName();
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
 
+function greetMe(name) {
+  console.log(name);
+}
 
+greetMe('Finley');
+greetMe('Lupin');
+greetMe('Paige');
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
 
+function sum(Num1, Num2) {
+  console.log(Num1 + Num2);
+}
 
+sum(2,4);
+sum(56,12);
+sum(52,9);
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
+function dinner(main, dessert) {
+  console.log('We are having ' + main + ' for dinner and ' + dessert + ' for dessert!' )
+}
 
+dinner('pasta', 'ice cream');
 
 //-------------------
 // PART 3: Naming is Hard
@@ -59,7 +82,7 @@ that it is reasonable to type, but descriptive enough that others can infer the 
 Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 What did you name each parameter, and why?
 
-EXPLAIN:
-
+I named the function dinner because dinner can include a main and dessert and it was an easy and descriptive
+way to categorize both.
 
 */
