738efc6d5aaea1fb235cc319c731971808630b8c
Steven Mancine <stevenrmancine@gmail.com>
2020-10-18 15:13:47 -0600

    Edit second to last exercise

--- section2/exercises/exercises/if-statements.js
+++ section2/exercises/exercises/if-statements.js
@@ -69,10 +69,10 @@ When numQuarters = 2, program should log "I have enough money for a gumball"
 When numQuarters = 3, program should log "I have enough money for a gumball"
 */
 
-var numQuarters = 2;
+var numQuarters = 1;
 
 // Write your conditional here
-if (numQuarters = (0 && 1)){
+if (numQuarters = (0 || 1)){
   console.log("I don't have enough money for a gumball");
 } else {
   console.log("I have enough money for a gumball");
