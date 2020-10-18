21fd45c7a5132a175691281431410a0d8c93eea6
Charles Morris <percworld@gmail.com>
2020-10-18 13:55:54 -0600

    Add Section 2 work

--- section2/exercises/exercises/functions.js
+++ section2/exercises/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -27,6 +27,15 @@ printName();
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
 
+function sayHello () {
+  console.log("Oh, Hello!");
+}
+
+sayHello();
+sayHello();
+sayHello();
+sayHello();
+
 //-------------------
 // PART 2: Arguments and Parameters
 //-------------------
@@ -35,10 +44,25 @@ printName();
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
 
+function greetMe(name) {
+  console.log(`${name}`);
+}
+
+greetMe ("Amelia");
+greetMe ("Jennifer");
+greetMe ("Charley");
+
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
+
+function logsSum(num1,num2) {
+  console.log(num1 + num2);
+}
+logsSum(2,44);
+logsSum(23,45);
+logsSum(8,15);
 
 
 
@@ -46,7 +70,11 @@ printName();
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
-
+function conCate(str1,str2) {
+  console.log("I hear that " + str1 + " likes " + str2 + "!");
+}
+conCate("Billy", "Miranda");
+conCate("Bobby", "Matilda");
 
 //-------------------
 // PART 3: Naming is Hard
@@ -61,5 +89,9 @@ What did you name each parameter, and why?
 
 EXPLAIN:
 
-
+I named the function conCate really only because I was researching why this is considered concatenation when the strings
+are separate and in the same sentence but I became a little frustrated because I think of concatenation as the combination
+of two strings into one new entity. It is perhaps not the greatest choice and I may be better off still saying what the
+function does but being a bit more descriptive about it's particular type of operation, like function whoFancies()
+or function hasCrush() 
 */
