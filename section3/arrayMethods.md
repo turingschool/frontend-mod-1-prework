83878a5b4ed1d1700397b44d5d11019f6fec9ccd
Charles Morris <percworld@gmail.com>
2020-10-21 13:30:43 -0600

    Update Session 3 and add arrayMethods.md

---
+++ section3/arrayMethods.md
@@ -0,0 +1,10 @@
+Array Methods:
+
+pop: Will remove the last item of an array and update length property.
+   fruits.pop()
+push: Will add an item to the end of an array and update length property. (Append)
+   fruits.push("Orange")
+shift: Will remove the first item of the array and update length property.
+   fruits.shift()
+unshift: Will add a new item at the beginning of an array and update length property. (Store)
+   fruits.unshift("Apple")
--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -23,25 +23,42 @@ for (var i = 0; i < 3; i++) {
 }
 
 // What is logged to the console for this second example? Why?
-// EXPLAIN:
+// EXPLAIN
+
+// A 0, then 1, then 2.  As the loop initializes i as 0, and i increments on each loop,
+// and the condition is met 3 times, the value of i is logged in each round.
 
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
 
+for (i = 1; i < 8; i++) {
+  console.log(2 + 2);
+}
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
 
+for (i = 1; i < 11; i++) {
+  console.log("She sells seashells down by the seashore");
+}
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
+
+for (i = 1; i < 4; i++) {
+  console.log("This is loop number: " + i);
+}
 
 // YOU DO: Using a for loop, how could you get an output that looks like this:
 
+
+for (i = 10; i != 0; i--) {
+  console.log(i);
+}
 // 10
 // 9
 // 8
@@ -53,6 +70,6 @@ for (var i = 0; i < 3; i++) {
 // 2
 // 1
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your pre-work group on Slack!
