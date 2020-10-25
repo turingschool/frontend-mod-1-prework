eba338056f5fdacde132611feafe366afb7bd472
Paige Vannelli <paigevannelli@gmail.com>
2020-10-25 10:56:35 -0600

    Add solutions to FizzBuzz and tenSpeckeledFrogs

---
+++ finalProject/fizzBuzz.js
@@ -0,0 +1,26 @@
+for (var i = 0; i < 101; i++) {
+  if(i % 3 === 0 && i % 5 ===0) {
+    console.log("FizzBuzz")
+  } else if(i % 3 === 0) {
+    console.log('Buzz')
+  } else if(i % 5 === 0) {
+    console.log('Fizz')
+  } else console.log(i)
+};
+
+// Code Challenge
+
+//Example log numbers 300 - 400
+
+for (var i = 300; i < 401; i++) {
+  if(i % 3 === 0 && i % 5 ===0) {
+    console.log("FizzBuzz")
+  } else if(i % 3 === 0) {
+    console.log('Buzz')
+  } else if(i % 5 === 0) {
+    console.log('Fizz')
+  } else console.log(i)
+};
+
+// You can change var i = to which ever index number you want to start at and change
+// i < the index number you'd like to stop at.
---
+++ finalProject/tenSpeckledFrogs.js
@@ -0,0 +1,23 @@
+
+for (var i = 10; i >= 1; i--) {
+  if (i >= 3) {
+  console.log(i + ' speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were ' + (i - 1) + ' speckled frogs.')
+} else if (i === 2) {
+  console.log(i + ' speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was ' + (i - 1) + ' speckled frog.')
+} else
+  console.log(i + ' speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!')
+};
+
+//Challenge: make the code work for any number of frogs
+// example: 25 frogs
+
+for (var i = 25; i >= 1; i--) {
+  if (i >= 3) {
+  console.log(i + ' speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were ' + (i - 1) + ' speckled frogs.')
+} else if (i === 2) {
+  console.log(i + ' speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was ' + (i - 1) + ' speckled frog.')
+} else
+  console.log(i + ' speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!')
+};
+
+//Challenge: print words in stead of numbers
