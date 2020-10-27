3c045aacaee8506da69b8e8d1e42591cf9a4b26b
Jim Charnesky <mijnesky@hotmail.com>
2020-10-27 12:13:40 -0600

    Add fully working for loop by eliminating a variable

--- finalProject/fizzBuzz.js
+++ finalProject/fizzBuzz.js
@@ -1,13 +1,30 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// I over did this one, but wanted to leave it for posterity
 
-for (var i = 0; i < numbers.length; i++) {
-  if (numbers[i] % 5 === 0 && numbers[i] % 3 === 0) {
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// for (var i = 0; i < 100; i++) {
+//   if (numbers[i] % 5 === 0 && numbers[i] % 3 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (numbers[i] % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (numbers[i] % 3 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(numbers[i]);
+//   }
+// }
+
+
+// Answer to fizzbuzz.md
+
+for (var i = 0; i < 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
     console.log("FizzBuzz");
-  } else if (numbers[i] % 5 === 0) {
+  } else if (i % 5 === 0) {
     console.log("Buzz");
-  } else if (numbers[i] % 3 === 0) {
+  } else if (i % 3 === 0) {
     console.log("Fizz");
   } else {
-    console.log(numbers[i]);
+    console.log(i);
   }
 }
