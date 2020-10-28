87ce24c9c4cacb7cd4bd5560e7bb1308adafe3b0
Melanie Daoheuang <daoh0031@gmail.com>
2020-10-28 17:18:35 -0600

    Update format

--- finalProject/tenSpeckledFrogs.js
+++ finalProject/tenSpeckledFrogs.js
@@ -5,7 +5,7 @@ function firstParagraph (firstFrog,secondFrog){
 firstParagraph(10,9);
 
 function secondParagraph (secondFrog,thirdFrog){
-  console.log(secondFrog," speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there was",thirdFrog, "speckled frog");
+  console.log(secondFrog," speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there was",thirdFrog, " speckled frogs");
 };
 secondParagraph (9,8);
 
@@ -13,31 +13,3 @@ function thirdParagraph (thirdFrog,fourthFrog){
   console.log(thirdFrog, " speckled frog sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there were", fourthFrog," more speckled frogs!");
 };
 thirdParagraph(8,7);
-
-//Extension 1, any number of frogs
-// function firstParagraph (firstFrog,secondFrog){
-//   console.log(firstFrog," speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there were", secondFrog, " speckled frogs");
-// };
-// firstParagraph(10,9);
-//
-// function secondParagraph (secondFrog,thirdFrog){
-//   console.log(secondFrog," speckled frogs sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there was",thirdFrog, "speckled frog");
-// };
-// secondParagraph (9,8);
-//
-// function thirdParagraph (thirdFrog,fourthFrog){
-//   console.log(thirdFrog, " speckled frog sat on a log \n eating some most delicious bugs. \n One jumped in the pool where its nice and cool, \n then there were", fourthFrog," more speckled frogs!");
-// };
-// thirdParagraph(8,7);
-//
-// //Extension 2, print word versions of line 1 and 4
-// 3 speckled frogs sat on a log eating some most delicious bugs.
-// One jumped in the pool where its nice and cool, then there were 2 speckled frogs.
-//
-// 2 speckled frogs sat on a log eating some most delicious bugs.
-// One jumped in the pool where its nice and cool,
-// then there was 1 speckled frog.
-//
-// 1 speckled frog sat on a log eating some most delicious bugs.
-// One jumped in the pool where its nice and cool,
-// then there were no more speckled frogs!
