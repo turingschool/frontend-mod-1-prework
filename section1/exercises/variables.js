10cf8d0ad96afef3c8a2fca495d25df80f06feea
Elizabeth Hahn <elizhahn87@gmail.com>
2020-10-30 15:23:29 -0600

    Edit mistakes in work

--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -18,7 +18,7 @@ console.log(name);
 // EXAMPLE: Write code below to add 2 to the variable `students` and
 // log the result:
 var enrolledStudents = 22;
-console.log(enrolledStudents);
+console.log(enrolledStudents + 2);
 
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
