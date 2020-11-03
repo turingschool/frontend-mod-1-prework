21fd0b79869a176ed46b98685d8cf7f146106742
Angela Battillo <angela.battillo@gmail.com>
2020-11-03 09:53:29 -0700

    Revise and consolodate for loop with else if statement

--- finalProject/10_speckled_frogs.js
+++ finalProject/10_speckled_frogs.js
@@ -54,6 +54,7 @@ for (var i = 0; i <= 10; i++) {
 */
 
 //After reviewing function video notes I switched around the incriminator for a decriminator and switched the values for the initializer and the condition with each other. The rest was experimenting with concatenating in repl.it
+/* 2nd speckled frog attempt:
 
 for (var i = 10; i > 0; i--)
      if (i > 8) {
@@ -63,6 +64,17 @@ for (var i = 10; i > 0; i--)
      } else if (i === 3) {
        console.log(i + " " + "speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were" + " " +(i - 1) + " " + "speckled frogs.")
      } else if (i === 2) {
+       console.log(i + " " + "speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were" + " " +(i - 1) + " " + "speckled frogs.")
+     } else {
+       console.log(1 + " "+ "speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where it's nice and cool, then there were no more speckled frogs!");
+     }*/
+
+//3rd Speckled frog review. revision to simplify use of else if statements.  this code as above only shows through the number 10. 
+
+     for (var i = 10; i > 0; i--)
+     if (i > 2) {
+       console.log(i + " " + "speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were" + (i - 1) + " " + "speckled frogs.");
+     } else if (i == 2) {
        console.log(i + " " + "speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were" + " " +(i - 1) + " " + "speckled frogs.")
      } else {
        console.log(1 + " "+ "speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where it's nice and cool, then there were no more speckled frogs!");
