40628adfcfe54575cdcd78b4eb79a66d60c1d3ad
Cameron Mackintosh <cbmackintosh@outlook.com>
2020-11-04 11:55:05 -0700

    Correct syntax error in loop, use integers instad of words

--- finalProject/tenSpeckledFrogs.js
+++ finalProject/tenSpeckledFrogs.js
@@ -1,7 +1,7 @@
 
 var num = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
 
-for (i = twenty; i > zero; i--) {
+for (i = 10; i > 0; i--) {
   if (i > 2) {
     console.log((num[i - 1]) + " speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where it was nice and cool, then there were " + (num[i-2]) + " speckled frogs.")
   } else if (i === 2) {
