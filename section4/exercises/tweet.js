89d20eac89e25ed246637acf0f28bd32f86532fd
Sarah Lane <sarahlane8@gmail.com>
2020-12-29 15:45:33 -0700

    Add correction to Section 4 tweets

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -16,14 +16,30 @@ constructor(author, content, timeStamp, numberOfLikes, comments) {
   this.numberOfLikes = numberOfLikes;
   this.comments = comments;
 }
+addLike() {
+  this.numberOfLikes++;
+}
+
+addComment(newComment) {
+  this.comments.push(newComment);
+}
+
 };
 
 var tweet1 = new Tweet ("Sarah", "Who wants to hear a daily new joke?", "Dec 12, 2020, 8:42am", 47, ["That's great!", "I do!"]);
-var tweet2 = new Tweet ("Eric", "Mod1 prework is due soon!", "Jan 5, 2021, 6:00pm", 0, ["Hope I don't mess it up!", "I'm so nervous!"]);
-var tweet3 = new Tweet ("Alice", "Congrats on finishing Mod1 prework!", "Jan 7, 2021, 7:00am", 163, ["We did it!", "Praise Jesus!"]);
-var tweet4 = new Tweet ("Ellen", "Welcome to Mod 1!", "Feb 1, 2021, 9:00am", 3026, ["Excited to get started!", "I'm going to be a coder!"]);
-
 console.log(tweet1);
+
+tweet1.addLike();
+console.log(tweet1);
+
+var tweet2 = new Tweet ("Eric", "Mod1 prework is due soon!", "Jan 5, 2021, 6:00pm", 0, ["Hope I don't mess it up!", "I'm so nervous!"]);
 console.log(tweet2);
+
+tweet2.addComment("What if I fail??");
+console.log(tweet2);
+
+var tweet3 = new Tweet ("Alice", "Congrats on finishing Mod1 prework!", "Jan 7, 2021, 7:00am", 163, ["We did it!", "Praise Jesus!"]);
 console.log(tweet3);
+
+var tweet4 = new Tweet ("Ellen", "Welcome to Mod 1!", "Feb 1, 2021, 9:00am", 3026, ["Excited to get started!", "I'm going to be a coder!"]);
 console.log(tweet4);
