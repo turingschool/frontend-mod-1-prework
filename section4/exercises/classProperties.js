6af793091a00c5f57c7cb45d339d9efe219bd2d5
Dustin <harbaughdustin@yahoo.com>
2021-01-04 13:49:11 -0500

    add Section 4 work

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -16,10 +16,36 @@ Make sure to run the file with node in your command line.
   // You do not need any proerties at this time!!
 
 // Prompt 1: Dog
-
+class Dog{
+  constructor(age, lengthInInches, color) {
+    this.age = age;
+    this.lengthInInches = length;
+    ths.color = color;
+  }
+}
+var lab = new Dog(3, 39, "yellow");
+var boxer = new Dog(7, 30, "white");
 // Prompt 2: Snack
+class Snack{
+  constructor(shape, texture, company) {
+    this.shape = shape;
+    this.texture = texture;
+    this.company = company;
+  }
+}
+var chips = new Snack("oval", "crunchy", "utz");
+var cookies = new Snack("circle", "chewy", "chipsahoy");
 
 // Prompt 3: Shirt
+class Shirt{
+  constructor(color, size, sleveLength) {
+    this.color = color;
+    this.size = size;
+    this.sleveLengthInInches = length;
+  }
+}
+var shortSleeve = new Shirt("black", "large", 6);
+var longSleeve = new Shirt("blue", "medium", 20);
 
 //-------------------
 // Part 2: Properties
@@ -33,11 +59,66 @@ Make sure to run the file with node in your command line.
   // Log that variable to the console.
 
 // Prompt 1: Dog
-
+class Dog{
+  constructor(age, lengthInInches, color) {
+    this.age = 3;
+    this.lengthInInches = 39;
+    ths.color = yellow;
+  }
+}
+var lab = new Dog();
+var boxer = new Dog(7, 30, "white");
+console.log(lab)
+class Dog{
+  constructor(age, lengthInInches, color) {
+    this.age = 7;
+    this.lengthInInches = 30;
+    ths.color = white;
+  }
+}
+var boxer = new Dog();
+console.log(boxer)
 // Prompt 2: Snack
+class Snack{
+  constructor(shape, texture, company) {
+    this.shape = oval;
+    this.texture = crunchy;
+    this.company = utz;
+  }
+}
+var chips = new Snack();
+console.log(chips)
 
+class Snack{
+  constructor(shape, texture, company) {
+    this.shape = circle;
+    this.texture = chewy;
+    this.company = chipsAHoy;
+  }
+}
+var cookies = new Snack();
+console.log(cookies)
 // Prompt 3: Shirt
+class Shirt{
+  constructor(color, size, sleveLength) {
+    this.color = black;
+    this.size = medium;
+    this.sleveLengthInInches = 6;
+  }
+}
+var shortSleeve = new Shirt();
+console.log(shortSleeve)
 
+class Shirt{
+  constructor(color, size, sleveLength) {
+    this.color = white;
+    this.size = large;
+    this.sleveLengthInInches = 20;
+  }
+}
+var longSleeve = new Shirt();
+
+console.log(longSleeve)
 //-------------------
 // Part 3: Dynamic Properties
 //-------------------
@@ -51,7 +132,38 @@ Make sure to run the file with node in your command line.
   // Log those variables to the console.
 
 // Prompt 1: Dog
-
+class Dog{
+  constructor(age, lengthInInches, color) {
+    this.age = age;
+    this.lengthInInches = length;
+    ths.color = color;
+  }
+}
+var lab = new Dog(3, 39, "yellow");
+var boxer = new Dog(7, 30, "white");
+console.log(lab)
+console.log(boxer)
 // Prompt 2: Snack
-
+class Snack{
+  constructor(shape, texture, company) {
+    this.shape = shape;
+    this.texture = texture;
+    this.company = company;
+  }
+}
+var chips = new Snack("oval", "crunchy", "utz");
+var cookies = new Snack("circle", "chewy", "chipsahoy");
+console.log(chips)
+console.log(cookies)
 // Prompt 3: Shirt
+class Shirt{
+  constructor(color, size, sleveLength) {
+    this.color = color;
+    this.size = size;
+    this.sleveLengthInInches = length;
+  }
+}
+var shortSleeve = new Shirt("black", "large", 6);
+var longSleeve = new Shirt("blue", "medium", 20);
+console.log(shortSleeve)
+console.log(longSleeve)
--- section4/reflection.md
+++ section4/reflection.md
@@ -1,21 +1,32 @@
 ## Section 4 Reflection
 
 1. How different did your workflow feel this week, considering we asked you to follow the Pomodoro technique?
+    not to bad. pomodoro breaks help me clear my head a little so when i come back i cant thing straight better
 
 1. Regarding the work you did around setting intentions in Step 1 of the Pomodoro technique - how did that go? Were you surprised by anything (did you find yourself way more focused than you realized, more distracted that you thought you'd be, estimating times accurately or totally off, etc)?
+    it was a little of both. it would help me focus but sometimes i took long breaks then i wanted to because i got distracted.
 
 1. What is an Object, and how is it different from an Array in Javascript?
-
+  an object is like an object for something. it shares a common key with things in it but every variable is different. an array just list things with a common key while an object goes more in depth about it.
 1. For each set of data, would an array or object be better to store it? Explain your choice.
 
   * List of all of the students in class
+      array because they all share the common trait of being in same class
   * List of states and their capitals
+      object because even though the share common trait they need more information(capital)
   * List of things to pack for vacation
+      array because its a list of things need and it can change
   * Names of all the Instagram accounts I follow
+      array because as time goes on things will be added to it
   * List of student names and their cohort
+      object because even though they are students they need more information on the cohort they are in
   * Ingredients and amount of each ingredient to bake a cake
+      object because the ingredients will be a key and how much would be a value
   * All my favorite restaurants
+      array because it is just naming restaurants and doesn't need more information
 
-1. In this section, we talked about an `transaction item object`. It has keys that represent different properties an bank transaction would have, and values that store the data. In a banking app, this data would eventually be displayed in the browser for the user. Think of another Object in a web application that you use frequently. What is it? What keys might be on it? What are example values? 
+1. In this section, we talked about an `transaction item object`. It has keys that represent different properties an bank transaction would have, and values that store the data. In a banking app, this data would eventually be displayed in the browser for the user. Think of another Object in a web application that you use frequently. What is it? What keys might be on it? What are example values?
+  with snapchat looking at most recent snaps it can be an object called snaps. with keys being name, streak, snapSent. name would be the string of the name. streak would be a integer and snap sent could be a boolean. 
 
 1. What questions do you still have about classes and/or Objects?
+is it possible to add a loop to a method?
