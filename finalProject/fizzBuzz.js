9499e620c06ac73b5f8857d58880837fef5a80a7
Chris Anderson <mistercanderson@gmail.com>
2021-01-07 14:06:00 -0700

    Refactor into functions

--- finalProject/fizzBuzz.js
+++ finalProject/fizzBuzz.js
@@ -1,26 +1,17 @@
-  var range = {
-    start: 1,
-    end: 100,
-    select: function(start, end) {
-      this.start = start;
-      this.end = end;
-    }
-  };
+//FIZZ BUZZ
 
-// activate the line below to select range parameters
-// range.select(3, 30);
-
-  for (i = range.start; i <= range.end; i++) {
+let makeItFizz = (start, end) => {
+  for (var i = start; i <= end; i++) {
     if ((i % 3 == 0) && (i % 5 == 0)) {
       console.log("FizzBuzz");
-    }
-    else if (i % 3 == 0) {
+    } else if (i % 3 == 0) {
       console.log("Fizz");
-    }
-    else if (i % 5 == 0) {
+    } else if (i % 5 == 0) {
       console.log("Buzz");
-    }
-    else {
+    } else {
       console.log(i);
     }
-  };
+  }
+};
+
+makeItFizz(5, 20);
--- finalProject/tenSpeckledFrogs.js
+++ finalProject/tenSpeckledFrogs.js
@@ -1,20 +1,15 @@
-var frogs = {
-  onLog: 10,
-  changeTotal (frogTotal) {
-    this.onLog = frogTotal
+//FROGS ON A LOG
+
+let logFrogs = (numOfFrogs) => {
+  for (var i = numOfFrogs; i >= 1; i--) {
+    if (i > 2) {
+      console.log(`${i} speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were ${i - 1} speckled frogs.`);
+    } else if (i > 1) {
+      console.log(`${i} speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was ${i - 1} speckled frog.`);
+    } else {
+      console.log(`${i} speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!`);
+    }
   }
 };
 
-frogs.changeTotal(15);
-
-for (i = frogs.onLog; i >= 1; i--) {
-  if (i > 2) {
-    console.log(i + " speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were " + (i - 1) + " speckled frogs.");
-  }
-  else if (i > 1) {
-    console.log(i + " speckled frogs sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there was " + (i - 1) + " speckled frog.");
-  }
-  else {
-    console.log(i + " speckled frog sat on a log eating some most delicious bugs. One jumped in the pool where its nice and cool, then there were no more speckled frogs!");
-  }
-};
+logFrogs(5);
