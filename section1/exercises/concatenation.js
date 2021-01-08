2fa10151333df512e44c9d775f58c03a73bb6ba2
Kendall Haworth <kendallhaw@gmail.com>
2021-01-08 11:15:58 -0800

    Add Section 1

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -22,7 +22,8 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here: String data. When the variables are concatenated
+// they will return a string so that the text and integers can be combined into a single data type.
 
 
 // YOU DO:
@@ -30,6 +31,9 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
+var string = "The " + speedy + " jumped over the " + slowPoke;
+
+console.log(string);
 
 
 // YOU DO:
@@ -40,16 +44,22 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy + "!");
+
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
+var leafCount = 7;
+var treeType = "aspen";
+var weather = "wind";
 
+console.log("All " + leafCount + " leaves on the " + treeType + " danced in the " + weather + ".");
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -23,34 +23,51 @@ console.log(enrolledStudents);
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
-
+var dobbySays = "Harry Potter must not return to Hogwarts!";
+console.log(dobbySays)
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
-
+var firstName = "Kendall";
+var isHungry = true;
+var numberOfPets = 0;
+console.log(firstName, isHungry, numberOfPets);
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
+// How did you decide to use the data type you did for each of the three variables above?
   // Explain.
+  /*
+For the first variable, I decided to use a string data type because generally first names are made up of text,
+and strings are the data type that can include letters and are used to convey text.
+For the second variable, isHungry is like asking the question "Are they hungry?", which invites a
+yes or no response. A yes/no response can also be answered as true or false. For this reason, I used
+a boolean data type for this variable.
+For the third variable, the variable name indicates that the value will be the number of pets, and you can't
+have a fraction of a pet. Therefore, the integer data type is the appropriate one to use.
+  */
 
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
-
+firstName = "Madison";
+isHungry = false;
+numberOfPets = 12;
+console.log(firstName, isHungry, numberOfPets);
 
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
+console.log(healthySnacks + junkFoodSnacks);
 
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-  // as you would expect? If you had issues with doing so, ask in your pre-work channel for 
+// Did you run this file in your terminal to make sure everything printed out to the console
+  // as you would expect? If you had issues with doing so, ask in your pre-work channel for
   // help ASAP.
