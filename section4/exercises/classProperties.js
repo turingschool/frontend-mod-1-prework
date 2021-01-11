0a7a86620d1d9f91a9f57c3b4149ae985e8808a2
Kendall Haworth <kendallhaw@gmail.com>
2021-01-11 12:13:11 -0800

    Add section4 exercises and reflection

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -16,10 +16,31 @@ Make sure to run the file with node in your command line.
   // You do not need any proerties at this time!!
 
 // Prompt 1: Dog
+class Dog {
+}
 
+var labrador = new Dog();
+var chihuaha = new Dog();
+console.log(labrador, chihuaha);
 // Prompt 2: Snack
+class Snack {
+
+}
+
+var goldfish = new Snack();
+var cherry = new Snack();
+console.log(goldfish);
+console.log(cherry);
 
 // Prompt 3: Shirt
+class Shirt {
+
+}
+
+var longSleeve = new Shirt();
+var collared = new Shirt();
+console.log(longSleeve);
+console.log(collared);
 
 //-------------------
 // Part 2: Properties
@@ -33,10 +54,44 @@ Make sure to run the file with node in your command line.
   // Log that variable to the console.
 
 // Prompt 1: Dog
+class Dog {
+  constructor() {
+    this.weight = "44 pounds";
+    this.sex = "female";
+    this.color = "white";
+  }
+}
+
+var husky = new Dog();
+console.log(husky);
+
 
 // Prompt 2: Snack
+class Snack {
+  constructor(color, brand, price) {
+    this.color = "orange";
+    this.brand = "Dole";
+    this.price = "$.80 each";
+
+  }
+}
+
+var tangerine = new Snack();
+console.log(tangerine);
 
 // Prompt 3: Shirt
+class Shirt {
+  constructor() {
+    this.size = "small";
+    this.material = "polyester";
+    this.style = "casual";
+  }
+}
+
+var crewNeckTee = new Shirt();
+console.log(crewNeckTee);
+
+
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -52,6 +107,48 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+  constructor(weight, sex, color) {
+    this.weight = weight;
+    this.sex = sex;
+    this.color = color;
+  }
+}
+
+var labrador = new Dog("58 pounds", "female", "black");
+console.log(labrador);
+
+var chihuaha = new Dog("11 pounds", "male", "tan");
+console.log(chihuaha);
+
 // Prompt 2: Snack
+class Snack {
+  constructor(color, brand, price) {
+    this.color = color;
+    this.brand = brand;
+    this.price = price;
+
+  }
+}
+
+var goldfish = new Snack("orange", "Nabisco", "$1.99/ 12 oz.");
+console.log(goldfish);
+
+var cherry = new Snack("purple", "Oregon Natural Farms", "$4.99/ pound");
+console.log(cherry)
 
 // Prompt 3: Shirt
+class Shirt {
+  constructor(size,material,style) {
+    this.size = size;
+    this.material = material;
+    this.style = style;
+  }
+}
+
+var longSleeve = new Shirt("medium", "cotton", "casual");
+console.log(longSleeve);
+
+
+var collared = new Shirt("large","silk","formal");
+console.log(collared);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -10,4 +10,34 @@ tweet object instances _behave_ as expected.
 
 class Tweet {
 
-}
+  constructor(author,content,time,likes,comments) {
+    this.author = author;
+    this.content = content;
+    this.timeStamp = time;
+    this.numberOfLikes = likes;
+    this.comments = comments
+  }
+
+  addLike() {
+    this.numberOfLikes = this.numberOfLikes + 1;
+  }
+
+
+  addComment() {
+    this.comments = [...this.comments, newComment];
+  }
+
+};
+
+var tweet1 = new Tweet("@kendallha","Why do I keep trying to type 'hellow' instead of 'hello'?","01-10-20 2:41 PM",15,["Your brain is broken.","Go to sleep!"]);
+console.log(tweet1);
+
+var tweet2 = new Tweet("@gobbluth","I've made a huge mistake","04-04-10 3:34 PM",13980,["Another one, Gob!?","It's an illusion, Michael!"]);
+console.log(tweet2);
+
+tweet1.addLike();
+console.log(tweet1);
+
+var newComment = "What a great show!"
+tweet2.addComment();
+console.log(tweet2);
