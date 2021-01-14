76e6addeab53f4affc79a74ead13cd6e05c59d77
Beth Meeker <bethm.meeker@gmail.com>
2021-01-14 08:26:16 -0700

    Add tweet.js updates

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -8,6 +8,9 @@ Create several instances of your Tweet and log them to the console. Make sure th
 tweet object instances _behave_ as expected.
 */
 
+var comments = ["All", "Previous", "Comments", "Here"];
+
+
 class Tweet {
   constructor(author, content, timeStamp, numberOfLikes, comments) {
     this.author = author;
@@ -16,12 +19,41 @@ class Tweet {
     this.numberOfLikes = numberOfLikes;
     this.comments = comments;
   }
+  increaseLikes() {
+    this.numberOfLikes = numberOfLikes+1;
+  };
+  decreaseLikes() {
+    this.numberOfLikes = numberOfLikes-1;
+  };
+  addComment() {
+    this.comments = comments.concat("New Comment");
+  };
+  showComments() {
+    this.comments = comments;
+  }
 }
 
 var tweet1 = new Tweet("Beth Meeker", "Help, I'm Tweeting!",
-"6:28 AM・Jan 10, 2021", [], []);
+"6:28 AM・Jan 10, 2021", numberOfLikes=5);
+tweet1.showComments();
+tweet1.increaseLikes();
 console.log(tweet1);
 
 var tweet2 = new Tweet("Zach Amdurer", "Now starts this point forward.",
-"10:38 AM・Jan 10, 2021", [], []);
+"10:38 AM・Jan 10, 2021", numberOfLikes=33, "New Comment");
+tweet2.decreaseLikes();
+tweet2.addComment();
 console.log(tweet2);
+
+var tweet3 = new Tweet("Joanna Schwab", "OMG What is happening?!",
+"1:18 PM ・Jan 11, 2121", numberOfLikes=3, "New Comment");
+tweet3.addComment();
+tweet3.showComments();
+tweet3.increaseLikes();
+console.log(tweet3);
+
+var tweet4 = new Tweet("Beth Meeker", "JoannaLikesYou Let us know!",
+"1:22 PM ・Jan 10, 2021", numberOfLikes=0);
+tweet4.showComments();
+tweet4.increaseLikes();
+console.log(tweet4)
