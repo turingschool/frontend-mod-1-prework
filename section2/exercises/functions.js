ab67647738efd8b0a6a22dba2ac89d580d8868ac
EllieAzaveda <elliemiller66@icloud.com>
2021-01-14 14:18:32 -0700

    Add Section 2 work

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -27,6 +27,13 @@ printName();
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
 
+function sayHello() {
+  console.log("Oh, Hello!")
+};
+
+sayHello();
+sayHello();
+
 //-------------------
 // PART 2: Arguments and Parameters
 //-------------------
@@ -35,18 +42,32 @@ printName();
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
 
-
+function greetMe(name) {
+  console.log(name);
+}
+greetMe("Ellie");
+greetMe("Skylar");
+greetMe("Rihanna");
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
 
+function addTwoNumbers(num1, num2){
+  console.log(num1 + num2);
+}
+addTwoNumbers(2,3);
+addTwoNumbers(100, 523);
+addTwoNumbers(1111, 8888);
 
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
-
+function concatStrings(str1, str2){
+  console.log(str1 + " and " + str2 + " like to practice coding!");
+}
+concatStrings("Ellie", "Skylar");
 
 //-------------------
 // PART 3: Naming is Hard
@@ -59,7 +80,10 @@ that it is reasonable to type, but descriptive enough that others can infer the 
 Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 What did you name each parameter, and why?
 
-EXPLAIN:
+EXPLAIN: I chose the names of the function and the parameters based on what the question was asking me to do.
+I wanted to remember that the goal of the function was to concatenate two strings, so I also added str1 and str2
+as my two Parameters so I would accurately write the function for what it was supposed to do.
+
 
 
 */
