e8d3de5ebb858a5403c32f60ac5f153ba076e5bd
Darla Evans <darlaevans2000@gmail.com>
2021-01-14 02:14:46 -0700

    Add fuctions answers

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,11 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+};
+sayHello();
+sayHello();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -35,18 +40,33 @@ printName();
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
 
+ function greetMe(name, middleInitial, lastName) {
+  console.log("Hi there, " + name + ' ' + middleInitial + ' ' + lastName);
+}
+greetMe("Darla", "L", "Evans");
+greetMe("Sam", "M", "Hall");
+greetMe("Dylan", "T", "Smith");
+
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
-
-
+function addNum(num1, num2){
+  console.log(num1 + num2);
+}
+addNum(2, 5);
+addNum(4, 434);
+addNum(74, 33);
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
+function enemies (name1, name2) {
+  console.log(name1 + " and " + name2 + " are the biggest enemies of one another!");
+}
 
-
+enemies("Darla", "Kylo");
+enemies("Vader", "Luke");
 
 //-------------------
 // PART 3: Naming is Hard
@@ -59,7 +79,7 @@ that it is reasonable to type, but descriptive enough that others can infer the 
 Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 What did you name each parameter, and why?
 
-EXPLAIN:
+EXPLAIN: I named it enemies because the status of the argument is about 2 people who are enemies.
 
 
 */
