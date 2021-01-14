c2c358a18b3a92c272a3083c5d42568f5356520d
Beth Meeker <bethm.meeker@gmail.com>
2021-01-14 15:56:39 -0700

    Add final concatenation.js format

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -17,9 +17,9 @@ console.log("The " + team + " are " + name + "'s favorite Quidditch team");
 
 // YOU DO: Read the next few lines of code...
 var numberOfCreatures = 7;
-var creatures = "unicorns";
+var creatures = 'unicorns';
 
-console.log("My zoo has " + numberOfCreatures + " " + creatures + "!");
+console.log('My zoo has ' + numberOfCreatures + ' ' + creatures + '!');
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
 // Your answer and explanation here: The console will log "My zoo has 7 unicorns!" because
@@ -31,18 +31,18 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!");
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "The quick red fox jumped over the lazy brown dog":
-var speedy = "quick red fox";
-var slowPoke = "lazy brown dog";
-console.log("The " + speedy + " jumped over the " + slowPoke);
+var speedy = 'quick red fox';
+var slowPoke = 'lazy brown dog';
+console.log('The ' + speedy + ' jumped over the ' + slowPoke);
 
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
 
-slowPoke = "tortoise";
-speedy = "hare";
-var result = "In a predictable result, the " + slowPoke + " beat the " + speedy + "!";
+slowPoke = 'tortoise';
+speedy = 'hare';
+var result = 'In a predictable result, the ' + slowPoke + ' beat the ' + speedy + '!';
 console.log(result);
 
 
@@ -50,9 +50,9 @@ console.log(result);
 // Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
-var name = "Beth";
+var name = 'Beth';
 var age = 39;
-var introInfo = "Hi, my name is " + name + "." + " I am " + age + " years old.";
+var introInfo = 'Hi, my name is ' + name + '.' + ' I am ' + age + ' years old.';
 console.log(introInfo);
 
 //-------------------
