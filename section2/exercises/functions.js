cfdd3f103d47935eb1167a4ef12e0e0b1f4fa0f8
Maria DelSignore <maria.ad27@gmail.com>
2021-01-13 22:41:26 -0700

    Initial commit

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,13 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+}
+
+sayHello();
+sayHello();
+
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -34,18 +41,36 @@ printName();
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
+function greetMe(name) {
+  console.log(name);
+}
+
+greetMe("Cooper");
+greetMe("Meka");
+greetMe("Greta");
 
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
+function math(first = 1, second = 2) {
+  const sum = first * second
+  console.log(sum);
+}
 
+math(first = 4);
+math(first = 22);
+math(second = 16);
 
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
+function beverages(drink1, drink2) {
+  console.log(drink1 + ' and ' + drink2 + ' are beverages that have caffeine.');
+}
 
+beverages('tea', 'coffee');
 
 
 //-------------------
@@ -60,6 +85,10 @@ Look at the code you wrote for the previous YOU DO🎈 - what did you name the f
 What did you name each parameter, and why?
 
 EXPLAIN:
-
+In the previous example I named my function beveraages.  I did this because I wanted
+the output to read "tea and coffee are beverages that have caffeine."  I pulled the
+function name from my sentence as a descriptive word of the items.  I named the
+parameters "drink1" and "drink2" because they are both tea and coffee are drinks
+that I wanted to include.
 
 */
