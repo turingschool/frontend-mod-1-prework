9b3ca3f1a6073a59d069188cebb2e499d4652aef
Andew Carlin <andrewcarlinbusiness@gmail.com>
2021-01-15 05:32:52 -0500

    Complete exercise classMethods.js

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,17 +7,17 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
 // The burrito class should have a method named `addTopping`.
@@ -27,14 +27,28 @@ Make sure to run the file with node in your command line.
 // Call the `addTopping` method on two burritos, then log the burritos to verify
 // that the proteins have been changed.
 
+
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, toppings) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings;
+    }
+    changeProtein() {
+      this.protein = "Pork";
+    }
+    addTopping(){
+      this.toppings.push("Jalapenos");
+    }
   }
+var beanAndCheese = new Burrito(false, "Beans", ["Special Sauce", "Salsa", "Queso"]);
+var carneAsada = new Burrito("Steak", "guacamole", ["Melted cheese", "Pico", "Salsa"]);
+var chickenRice = new Burrito("Chicken", "Rice", ["Spicy sauce", "Melted cheese", "Pico"]);
 
-  // ADD CODE
-};
+beanAndCheese.changeProtein();
+carneAsada.addTopping();
+chickenRice.addTopping();
 
-// ADD CODE
-
+console.log(beanAndCheese);
+console.log(carneAsada);
+console.log(chickenRice);
