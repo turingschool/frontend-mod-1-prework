bf906e247878c3a7c9b0130e8ad2d44db58fda5d
mcraig2342 <matthewcookecraig10@gmail.com>
2021-01-15 14:21:24 -0700

    Update final project

---
+++ finalProject/tenSpeckledFrogs.js
@@ -0,0 +1,12 @@
+for (i = 10; i > 0; i--){
+  if (i >= 3){
+    console.log(i + " speckled frogs sat on a log eating some most delicious bugs."
+    + "One jumped in the pool where its nice and cool, then there were " + (i - 1) + " speckled frogs.");
+  } else if (i === 2) {
+    console.log(i + " speckled frogs sat on a log eating some most delicious bugs."
+    + "One jumped in the pool where its nice and cool, then there was " + (i - 1) + " speckled frog.");
+  } else {
+    console.log(i + " speckled frog sat on a log eating some most delicious bugs."
+    + "One jumped in the pool where its nice and cool, then there were no more speckled frogs.");
+  }
+}
