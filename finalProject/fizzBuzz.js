06faec9a6629445c89b5676d1876069853914cc0
Michann Stoner <stoner.michann@gmail.com>
2021-01-14 18:43:56 -0700

    Refactor code in fizzBuzz

--- finalProject/fizzBuzz.js
+++ finalProject/fizzBuzz.js
@@ -4,17 +4,13 @@
 // For any number that is a multiple of both 3 and 5, log 'FizzBuzz'
 // For all other numbers, log the number
 
-var multiples = 0;
 
 for (var i = 1; i < 101; i++) {
    if (i % 3 === 0 && i % 5 === 0) {
-     multiples += i;
      console.log("FizzBuzz");
   }else if (i % 3 === 0) {
-    multiples += i;
     console.log("Fizz");
   }else if (i % 5 === 0) {
-    multiples += i;
     console.log("Buzz");
   }else{
     console.log(i);
