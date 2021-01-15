e74c3608ea48d66d7600be9040effbd9c4310e75
mcraig2342 <matthewcookecraig10@gmail.com>
2021-01-15 14:29:08 -0700

    Update section 4

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,17 +7,17 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
 // The burrito class should have a method named `addTopping`.
@@ -28,13 +28,27 @@ Make sure to run the file with node in your command line.
 // that the proteins have been changed.
 
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, toppings) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings;
   }
-
+  changeProtein() {
+    this.protien = "ground beef";
+  }
+  addTopping() {
+    this.toppings[3] = "jalepenos"
+  }
   // ADD CODE
 };
 
+var chickenBurrito = new Burrito ("chicken", "wheat", ["cheese", "sour cream", "beans"])
+var veggieBurrito = new Burrito ("mushrooms", "white", ["rice", "corn", "salsa"])
+var steakBurrito = new Burrito ("steak", "wheat", ["salsa", "beans", "sour cream"])
 // ADD CODE
-
+chickenBurrito.changeProtein();
+console.log(chickenBurrito);
+veggieBurrito.addTopping();
+steakBurrito.addTopping();
+console.log(veggieBurrito);
+console.log(steakBurrito);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,38 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
+  constructor(author, content, timeStamp, numberOfLikes, comments) {
+    this.author = author;
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = numberOfLikes;
+    this.comments = comments;
 
+  }
+incrementLikes() {
+  this.numberOfLikes = 1 + this.numberOfLikes;
 }
+addComment() {
+  this.comments.push("new comment");
+}
+}
+var tweetOne = new Tweet ("Tim Jones", "I like turtles", "12:23 pm", 1, ["nice tweet","cool"]);
+console.log(tweetOne)
+tweetOne.incrementLikes();
+console.log(tweetOne);
+tweetOne.addComment();
+console.log(tweetOne);
+
+var tweetTwo = new Tweet ("Mark Smith", "So you're telling me a shrimp fried this rice?", "9:30 am", 598756, ["very original", "banger"]);
+console.log(tweetTwo)
+tweetTwo.incrementLikes();
+console.log(tweetTwo);
+tweetTwo.addComment();
+console.log(tweetTwo);
+
+var tweetThree = new Tweet ("Jonh Brown", "Idk what to make this tweet", "3:40 pm", 0, ["lame", "booo!"]);
+console.log(tweetThree)
+tweetThree.incrementLikes();
+console.log(tweetThree);
+tweetThree.addComment();
+console.log(tweetThree);    
