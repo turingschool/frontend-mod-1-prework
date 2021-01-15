011e91e8cb5e94eecd591b1b66f46ce7e163af89
Andew Carlin <andrewcarlinbusiness@gmail.com>
2021-01-15 04:03:59 -0500

    Complete classProperties.js exercise

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -17,10 +17,32 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+/* class Dog {
+
+}
+var poodle = new Dog();
+console.log(poodle);
+var pitbull = new Dog();
+console.log(pitbull)
 // Prompt 2: Snack
+class Snack {
+
+}
+var chips = new Snack();
+console.log(chips);
+var gronolaBar = new Snack();
+console.log(gronolaBar);
+
 
 // Prompt 3: Shirt
 
+class Shirt {
+
+}
+var crewNeck = new Shirt();
+console.log(crewNeck);
+var vNeck = new Shirt();
+console.log(vNeck);
 //-------------------
 // Part 2: Properties
 //-------------------
@@ -33,11 +55,38 @@ Make sure to run the file with node in your command line.
   // Log that variable to the console.
 
 // Prompt 1: Dog
+class Dog {
+  constructor(){
+    this.size = "Big";
+    this.color = "black";
+    this.age = 3;
+  }
+}
+var poodle = new Dog();
+console.log(poodle);
 
 // Prompt 2: Snack
+class Snack {
+  constructor(){
+    this.flavor = "Chocolate";
+    this.calories = 100
+    this.vegan = true;
+  }
+}
+var chips = new Snack();
+console.log(chips);
 
 // Prompt 3: Shirt
-
+class Shirt {
+  constructor(){
+    this.size = "Small";
+    this.color = "Black"
+    this.price = 10
+  }
+}
+var crewNeck = new Shirt();
+console.log(crewNeck);
+*/
 //-------------------
 // Part 3: Dynamic Properties
 //-------------------
@@ -51,7 +100,46 @@ Make sure to run the file with node in your command line.
   // Log those variables to the console.
 
 // Prompt 1: Dog
+//I tested all code below in another file because this node says the identifiers are already declared.
+class Dog {
+  constructor(size, color, age){
+    this.size = size;
+    this.color = color;
+    this.age = age;
+  }
+}
+var poodle = new Dog("Small", "White", "Old");
+console.log(poodle);
+
+var pitbull = new Dog("Medium", "Black", "Young");
+console.log(pitbull);
+
 
 // Prompt 2: Snack
+class Snack {
+  constructor(flavor, calories, isVegan){
+    this.flavor = flavor;
+    this.calories = calories;
+    this.isVegan = isVegan;
+  }
+}
+var chips = new Snack("BBQ", 100, false);
+console.log(chips);
+
+var gronolaBar = new Snack("Chocolate", 120, true);
+console.log(gronolaBar);
+
 
 // Prompt 3: Shirt
+class Shirt {
+  constructor(size, color, price){
+    this.size = size;
+    this.color = color;
+    this.price = price;
+  }
+}
+var crewNeck = new Shirt("XL", "Blue", 10);
+console.log(crewNeck);
+
+var vNeck = new Shirt("XS", "Pink", 12);
+console.log(vNeck);
