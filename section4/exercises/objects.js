f83834266494df3135bf7bf981fb1f59bb070bb0
Darla Evans <darlaevans2000@gmail.com>
2021-01-15 03:59:51 -0700

    Add exercises answers

--- section4/exercises/objects.js
+++ section4/exercises/objects.js
@@ -17,30 +17,42 @@ var foods = {
 };
 
 console.log(foods);
-// Notice that the variable name is foods, plural, because it has the ability to hold info about 
-// potentially many foods. That plural name indicates to another developer that the variable stores 
+// Notice that the variable name is foods, plural, because it has the ability to hold info about
+// potentially many foods. That plural name indicates to another developer that the variable stores
 // either an Array or an Object.
 
 //-------------------
 // Part 1: Zoo Animals
 //-------------------
 
-// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and 
+// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and
 // the number of that type of animal at the zoo.
+var animals = {
+  giraffes: 18,
+  owls: 4,
+  tigers: 7,
+  zebras: 6
+};
+
 
 
 // Using the zoo that you created above, log the value of the first item in
 // the Object
-
+console.log(animals.giraffes);
 
 // Add an animal to the zoo Object, and log the updated Object.
+animals.donkeys=3;
 
+console.log(animals);
 
 // Change the value for at least one of your animal keys, and log the updated Object.
-
+animals.tigers = 10,
+animals.giraffes = 19
+console.log(animals);
 
 // Add another animal to the zoo Object, and log the updated Object.
-
+animals.penguins=17
+console.log(animals);
 
 //-------------------
 // Part 2: Email
@@ -55,7 +67,15 @@ value should be some appropriate value for that key. Work to have at least 5 key
 
 Log your email object to the console.
 */
+var email = {
+  subject: "Huge Sale!",
+  sender:"clothingshop@clothing.com",
+  to:"darlaevans2000@gmail.com",
+  time:"15:30",
+  attachment: true
 
+};
+console.log(email);
 //-------------------
 // Part 3: Many Emails - OPTIONAL EXTENSION
 //-------------------
@@ -70,7 +90,7 @@ In the arrays exercise, we gave the following example of an array of Instagram p
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 /*
-Frankly, that was a very simplified version of the Array the Instagram developers have 
+Frankly, that was a very simplified version of the Array the Instagram developers have
 written and work with. Still probably slightly simplified as we don't know what their code
 actually looks like, but it may look more like this:
 */
@@ -82,7 +102,7 @@ var posts = [
     timeStamp: "4:37 PM August 13, 2019",
     numberLikes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -96,12 +116,36 @@ console.log(posts);
 console.log(posts[0]);
 
 /*
-The code snippet above shows an Array with 2 elements. Each element in an 
-Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+The code snippet above shows an Array with 2 elements. Each element in an
+Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 good to have some exposure before going into Mod 1.
 */
 
-// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 // key-value pairs you used in your email Object above.
 // Then, log the email Array to the console.
+var emails = [
+  {
+    subject: "Huge Sale!",
+    sender:"clothingshop@clothing.com",
+    to:"darlaevans2000@gmail.com",
+    time:"15:30",
+    attachment: true
+  },
+  {
+    subject: "Fraud Alert",
+    sender:"bank@money.com",
+    to:"darlaevans2000@gmail.com",
+    time:"17:35",
+    attachment: false
+  },
+  {
+    subject: "Sign in alert",
+    sender:"instagram@instagram.com",
+    to:"darlaevans2000@gmail.com",
+    time:"03:45",
+    attachment: true
+  }
+]
+console.log(emails);
