a25f8eb7574c51550cf39832b92eea7539b73753
EllieAzaveda <elliemiller66@icloud.com>
2021-01-16 00:31:10 -0700

    Still trying to make this work

---
+++ finalProject/ceasarCipher.js
@@ -0,0 +1,15 @@
+//encode("Hello World", 5); //should log "CZGGJ RJMGY"
+
+var stringOne = "Hello World";
+
+for (i = 0; i < stringOne.length; i++){
+  if ()
+}
+
+
+//encode("Hi", 2); //should log "FG"
+
+var stringTwo = "Hi";
+for (i = 0; i < stringTwo.length; i++){
+
+}
