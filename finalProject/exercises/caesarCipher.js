9d4c8a16dc74b4f9e259df87f7776c41ce39eef6
Jon Schlandt <schlanjo@gmail.com>
2021-01-26 18:24:01 -0700

    Incorporate 'getCipherArray()' method for improved encoding

--- finalProject/exercises/caesarCipher.js
+++ finalProject/exercises/caesarCipher.js
@@ -2,14 +2,16 @@ class CaeserCipher {
 	constructor(originString, shiftAmount) {
 		// The following property is the originating String that will be encoded
 		this.originString = originString;
+		this.shiftAmount = shiftAmount;
 		// The following property is the encoded String returned upon "encode()"
 		this.encodedString = "";
-		this.shiftAmount = shiftAmount;
-		// The following property is used to determine what a given letter will
-		// encode to based upon the "shiftAmount" value.
+		// The following property will be referenced to determine what a given
+		// letter will encode to based upon the "shiftAmount" value.
 		this.alphaRef = {
-			lower: "abcdefghijklmnopqrstuvwxyz",
-			upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+			lower: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+			        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+			upper: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+			        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 		};
 	}
 
@@ -22,11 +24,13 @@ class CaeserCipher {
 		for (var i = 0; i < this.originString.length; i++) {
 			originLetter = this.originString[i];
 
-			// The following logic determines how the "getEncodedLetter" method is
-			// called, and assigns its return value to "encodedLetter" - this value
-			// is then concatenated with "encodedString". If the value of
-			// "originLetter" is a space or a special character, it is directly
-			// concatenated with "encodedString" as these don't need encoding.
+			/*
+			The following logic determines how the "getEncodedLetter" method is
+			called, and assigns its return value to "encodedLetter" - this value
+			is then concatenated onto "encodedString". If the value of
+			"originLetter" is a space or a special character, it is directly
+			concatenated onto "encodedString" as these don't need encoding.
+			*/
 			if (originLetter === " ") {
 				this.encodedString += originLetter;
 			} else if (this.alphaRef.upper.includes(originLetter)) {
@@ -47,38 +51,48 @@ class CaeserCipher {
 
 	getEncodedLetter(originLetter, isUpper) {
 		var originIndex;
-		var encodeIndex;
 		var encodedLetter;
 
-		// The following logic references either the "upper" or "lower" String of
-		// "alphaRef" (depending on whether "isUpper" is true or false) to find
-		// the encoded version of the originating letter based upon the
-		// "shiftAmount" value provided.
-		if (isUpper === true) {
+		/*
+	  The following logic references either the "upper" or "lower" Array of
+		"alphaRef" (depending on whether "isUpper" is true or false) to determine
+		how the "getCipherArray()" method is called. The "getCipherArray()" method
+		returns a ciphered version of the "upper" or "lower" array.
+		*/
+		if (isUpper) {
 			originIndex = this.alphaRef.upper.indexOf(originLetter);
-			encodeIndex = originIndex - this.shiftAmount;
-			// In the "if" statement below, I'm adding 26 to any encode indexes that
-			// calculate as negative to prevent undefined values. For example, if the
-			// "originLetter" is "c" (index of 2 in "alphaRef.lower") and the
-			// "shiftAmount" is 3, then the "encodeIndex" is -1.
-			if (encodeIndex < 0) {
-				encodeIndex += 26;
-			}
-			encodedLetter = this.alphaRef.upper[encodeIndex];
-		} else if (isUpper === false) {
+			encodedLetter = this.getCipherArray(true)[originIndex];
+		} else if (!(isUpper)) {
 			originIndex = this.alphaRef.lower.indexOf(originLetter);
-			encodeIndex = originIndex - this.shiftAmount;
-			if (encodeIndex < 0) {
-				encodeIndex += 26;
-			}
-      encodedLetter = this.alphaRef.lower[encodeIndex];
+      encodedLetter = this.getCipherArray(false)[originIndex];
 		}
 
 		// Once the encoded letter has been found, it is returned (and used in the
 		// "encode()" method).
 		return encodedLetter;
 	}
+
+	getCipherArray(isUpper) {
+		var cipherArray;
+
+		// Assign the appropriate "alphaRef" Array based upon argument passed
+		// during call.
+		if (isUpper) {
+			cipherArray = this.alphaRef.upper;
+		} else if (!(isUpper)) {
+			cipherArray = this.alphaRef.lower;
+		}
+
+		// Shift the letters, by the "shiftAmount", in the "alphaRef" Array
+		// to the right
+		for (var i = 1; i <= this.shiftAmount; i++) {
+			cipherArray.unshift(cipherArray.pop());
+		}
+
+		// Return the ciphered Array once completely shifted
+		return cipherArray;
+	}
 }
 
-var testCipher = new CaeserCipher("My name is Jon.", 5);
-console.log(testCipher.encode());
+var cipherTest = new CaeserCipher("This is a test!", 3);
+console.log(cipherTest.encode());
