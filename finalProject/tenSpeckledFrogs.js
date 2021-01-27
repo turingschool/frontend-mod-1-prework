51862fe76a346bc0f53867da34a561c2d8457fab
Maria DelSignore <maria.ad27@gmail.com>
2021-01-26 19:15:06 -0700

    Accomplish tenSpeckledFrogs

---
+++ finalProject/tenSpeckledFrogs.js
@@ -0,0 +1,20 @@
+var line1 = ' speckled frogs sat on a log'
+var line2 = 'eating some most delicious bugs.'
+var line3 = 'One jumped in the pool where its nice and cool,'
+var line4 = 'then there '
+
+for (var i = 11; i >= 4; i--) {
+  console.log((i - 1).toString() + line1)
+  console.log(line2);
+  console.log(line3);
+  console.log(line4 + 'were ' + (i - 1 - 1).toString() + ' speckled frogs.')
+} if ((i <= 3) || (i = 1)) {
+  console.log((i - 1).toString() + line1);
+  console.log(line2);
+  console.log(line3);
+  console.log(line4 + 'was ' + (i - 1 - 1).toString() + ' speckled frog.');
+  console.log((i - 1 - 1).toString() + ' speckled frog sat on a log');
+  console.log(line2);
+  console.log(line3);
+  console.log(line4 + 'no more speckled frogs!');
+}
