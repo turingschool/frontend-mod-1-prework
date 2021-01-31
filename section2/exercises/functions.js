29d230341a55d17feb9d360d0b9071f42d70174b
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-01-30 17:12:33 -0700

    Add text to functions.js

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -25,7 +25,15 @@ printName();
 
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
+
+function sayHello() {
+  console.log("Oh, Hello!");
+};
+
 // Then, call the function 2 times.
+
+sayHello();
+sayHello();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -33,19 +41,40 @@ printName();
 
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
+
+function greetMe(name) {
+  console.log("Hi " + name);
+};
+
 // Then, call the function 3 times, each time, passing it a different name.
 
+greetMe("Janna");
+greetMe("Sophie");
+greetMe("Miriam");
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
+
+function addNums(num1, num2) {
+  console.log(num1 + num2);
+};
+
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
 
-
+addNums(39, 88);
+addNums(.025, 101);
+addNums(-72, 88);
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
+
+function eatSnacks(snack1, snack2) {
+  console.log("I am hungry for " + snack1 + " and maybe also " + snack2 + "...")
+};
+
+eatSnacks("popcorn", "cookies");
 
 
 //-------------------
@@ -61,5 +90,6 @@ What did you name each parameter, and why?
 
 EXPLAIN:
 
+I named my function eatSnacks partially because I looked at the previous examples and emulated their naming conventions. printName(), for example, used a verb as its first word and a noun for its second. I also realized the noun could also be used as a parameter in the function. I named my parameters snack1 and snack2 because they directly related to the function name eatSnacks, and thus felt very precise. 
 
 */
