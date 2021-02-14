a3bc713bb37f8a1fe2fc2dab6f739c2142e9ead1
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-02-14 11:22:25 -0700

    Add text to tweet.js

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,40 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
-
+  constructor(author, content, timeStamp, numberOfLikes, comments) {
+    this.author = author;
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = numberOfLikes;
+    this.comments = comments;
+  }
+  addLike() {
+    this.numberOfLikes++
+  }
+  addComment(addComment) {
+    this.comments.push(addComment);
+  }
 }
+
+var tyTweet = new Tweet("Ty Holter", "You betchum bobcats!", "2:17 PM MST", 4, ["Wow! I love bobcats!", "As if...", "I like your attitude"]);
+
+tyTweet.addLike();
+
+tyTweet.addComment("Go Ty! You tell 'em!");
+
+console.log(tyTweet);
+
+var ashTweet = new Tweet("Ash Oh", "I'm no scaredy cat!", "5:40 AM MST", 88, ["Humble brag, much", "no way! you're a scaredy RAT!", "I believe in you!"]);
+
+ashTweet.addLike();
+
+ashTweet.addComment("I'll see you tomorrow!");
+
+console.log(ashTweet);
+
+var ratTweet = new Tweet("JuneBugTheRat", "mOrE ChEErIoS, PlEaSE", "6:17 PM MST", 10999, ["JuneBugTheRat is the modern day Nostradamus", "I've never met such a sentient rodent", "JuneBugTheRat for president 2024!!!!11"]);
+
+ratTweet.addLike();
+
+
+console.log(ratTweet);
