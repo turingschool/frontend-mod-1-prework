6a859ac27a5b2ad491eac1745218c639ef89a8b8
Dean Cook <deancook371@gmail.com>
2021-02-14 15:32:40 -0700

    Add Section 2 work

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,12 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+}
+
+sayHello();
+sayHello();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -34,19 +40,32 @@ printName();
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
+function greetMe(name) {
+  console.log(name);
+}
 
-
+greetMe("Dean");
+greetMe("Matt");
+greetMe("August");
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
+function logSum(num1, num2) {
+  console.log(num1 + num2);
+}
 
-
+logSum(2, 4);
+logSum(1, 1);
+logSum(5, 5);
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
+function kissingSong(name1, name2) {
+  console.log(name1 + " and " + name2 + ", kissing in a tree. K I S S I N G!");
+}
 
-
+kissingSong("John", "Sally");
 
 //-------------------
 // PART 3: Naming is Hard
@@ -60,6 +79,10 @@ Look at the code you wrote for the previous YOU DO🎈 - what did you name the f
 What did you name each parameter, and why?
 
 EXPLAIN:
-
+I named the function kissingSong, because I'd decided to go with a format that already concatenates two names--
+the classic "kissing in a tree" song to mock two people. I could probably be a little more descriptive, but I
+went shorter instead knowing that I would easily be able to tell what this function was for. I just called the
+name parameters "name1" and "name2" -- it's simple, but descriptive enough that I can work with them without
+getting confused.
 
 */
