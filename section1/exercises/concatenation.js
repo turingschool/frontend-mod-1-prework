d64e851f42f517833c85997afd13da711934d965
Hayley Witherell <hayleywitherell@gmail.com>
2021-02-13 19:24:57 -0700

    Add Section 1

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -22,8 +22,9 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:
 
+It becomes a string, because it's a combination of characters into a phrase.
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -31,6 +32,7 @@ console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
 
+console.log("The " + speedy + " jumped over the " + slowPoke)
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -39,13 +41,22 @@ var slowPoke = "lazy brown dog";
 
 slowPoke = "tortoise";
 speedy = "hare";
-
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy + " !")
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
 
+  var favoriteFruit = "strawberry";
+  var favoriteVegetable = "potato";
+  var favoriteChips = "Doritos"
+
+  var favoriteFruit = "strawberry";
+var favoriteVegetable = "potato";
+var favoriteChips = "Doritos"
+
+console.log("I want to eat a " + favoriteFruit + ", a " + favoriteVegetable + ", and a bag of " + favoriteChips + ".");
 
 //-------------------
 // FINAL CHECK
