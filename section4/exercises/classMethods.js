a581e452d5056a7c8bef972d3c8d892d4f4cd8ef
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-02-14 11:11:16 -0700

    Add text to classMethods.js

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,17 +7,17 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
 // The burrito class should have a method named `addTopping`.
@@ -28,13 +28,32 @@ Make sure to run the file with node in your command line.
 // that the proteins have been changed.
 
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, toppings) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings;
   }
-
-  // ADD CODE
+    // ADD CODE
+changeProtein(proteinAlternative) {
+  this.protein = proteinAlternative;
+}
+   addTopping(extraTopping) {
+ this.toppings.push(extraTopping);
+ }
 };
 
 // ADD CODE
+var veggieBurrito = new Burrito("tofu", "beans", ["guacamole", "salsa", "yogurt"]);
 
+var beefBurrito = new Burrito ("beef", "rice", ["hot sauce", "sour cream"]);
+
+var childBurrito = new Burrito ("cheese", "refried beans", ["guacamole", "green chili"]);
+
+childBurrito.changeProtein("chicken");
+console.log(childBurrito);
+
+veggieBurrito.addTopping("cheese");
+console.log(veggieBurrito);
+
+beefBurrito.addTopping("lime");
+console.log(beefBurrito);
