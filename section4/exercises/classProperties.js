62bc35771efadd2c88fcf1ac4d216b449b1a44c6
Shauna Myers <shaunamyers26@gmail.com>
2021-02-16 15:49:04 -0600

    Complete section 4 exercises

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -17,9 +17,39 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+
+}
+
+var schnauzer = new Dog();
+var greatDane = new Dog();
+
+console.log(schnauzer);
+console.log(greatDane);
+
 // Prompt 2: Snack
 
+class Snack {
+
+}
+
+var chips = new Snack();
+var pretzel = new Snack();
+
+console.log(chips);
+console.log(pretzel);
+
 // Prompt 3: Shirt
+
+class Shirt {
+
+}
+
+var longSleeve = new Shirt();
+var vNeck = new Shirt();
+
+console.log(longSleeve);
+console.log(vNeck);
 
 //-------------------
 // Part 2: Properties
@@ -34,9 +64,45 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+  constructor() {
+    this.puppy = 1;
+    this.adult = 5;
+    this.elderly = 10;
+  }
+}
+
+var poodle = new Dog();
+
+console.log(poodle);
+
 // Prompt 2: Snack
 
+class Snack {
+  constructor() {
+    this.licorice = 'sweet';
+    this.crackers = 'cheesy';
+    this.chips = 'salty';
+  }
+}
+
+var cookie = new Snack();
+
+console.log(cookie);
+
 // Prompt 3: Shirt
+
+class Shirt {
+  constructor() {
+    this.polo = 'striped';
+    this.tankTop = 'blue';
+    this.thermal = 'thick';
+  }
+}
+
+var turtleNeck = new Shirt();
+
+console.log(turtleNeck);
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -52,6 +118,48 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+  constructor(breed, years, mood) {
+    this.breed = breed;
+    this.age = years;
+    this.disposition = mood;
+  }
+}
+
+var rescue = new Dog('German Shepherd', 4, 'Tolerant');
+var myDog = new Dog('Akita', 12, 'Alpha');
+
+console.log(rescue);
+console.log(myDog);
+
 // Prompt 2: Snack
 
+class Snack {
+  constructor(flavor, size, name) {
+    this.taste = flavor;
+    this.ounces = size;
+    this.type = name;
+  }
+}
+
+var donut = new Snack('Maple', 12, 'Maple Log');
+var biscotti = new Snack('Vanilla', 6, 'Nabisco Wafer');
+
+console.log(donut);
+console.log(biscotti);
+
 // Prompt 3: Shirt
+
+class Shirt {
+  constructor(material, brand, price) {
+    this.fabric = material;
+    this.brand = brand;
+    this.cost = price;
+  }
+}
+
+var buttonUp = new Shirt('Flannel', 'Columbia', 65);
+var dressShirt = new Shirt('Silk', 'Guess', 125);
+
+console.log(buttonUp);
+console.log(dressShirt);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,31 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
+  constructor(author, content, timeStamp, numberOfLikes, comments) {
+    this.author = author;
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = numberOfLikes;
+    this.comments = comments;
+  }
 
+  incrementLikes() {
+    this.numberOfLikes += 150;
+  }
+
+  addComments() {
+    this.comments.push('How do you drink your coffee black?!');
+  }
 }
+
+var firstTweet = new Tweet('Sam Jones', 'Picture of cat wearing shirt', '1:23pm', 123, ['I have that same shirt.', 'I do not think this is very funny',
+'you should take more photos with your cat']);
+
+var secondTweet = new Tweet('Carmen Rosas', 'Picture of coffee with text', '7:36am', 201, ['You are up early', 'How much coffee do you drink in one day?',
+'You are my fav person ever!']);
+
+firstTweet.incrementLikes();
+console.log(firstTweet);
+
+secondTweet.addComments();
+console.log(secondTweet);
