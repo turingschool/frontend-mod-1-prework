c3b3f47a794907f7cadcc9af0070058104bf9ac0
Julie Zeiger <julieann.zeiger@gmail.com>
2021-02-17 21:48:05 +0100

    Add updated Mod 1 Prework

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -25,18 +25,18 @@ buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 //FizzBuzz
 function fizzBuzz(num1, num2, range) { //the name of the function is fizzBuzz and it has 3 parameters
-  for (var i = 0; i <= range; i++) { //this is a for loop; var is called i which starts at 0; the loop will continue as long as i is less than or equal to range; it will incerement by one each time it loops.
-    if (i % num1 === 0 && i % num2 === 0) {
-      console.log('fizzbuzz')
-    } else if (i % num1 === 0) {
+  for (var i = 0; i <= range; i++) { //this is a for loop; variable is called i which starts at 0; the loop will continue as long as i is less than or equal to range; it will incerement by one each time it loops.
+    if (i % num1 === 0 && i % num2 === 0) { //if i has a remainder for num1 that is equal to 0 and if i has remainder of for num2 that is equal to 0
+      console.log('fizzbuzz') //log the value in strings
+    } else if (i % num1 === 0) { // else do this instead
       console.log('fizz')
-    } else if (i % num2 === 0) {
+    } else if (i % num2 === 0) { //else do this instead
       console.log('buzz')
-    } else {
-      console.log(i)
+    } else { // else do this as the last resort
+      console.log(i) //log the variable i 
     }
   }
-}
+} //function closing
 
-fizzBuzz(3, 5, 100) //calling in function name and giving it 3 arguments
-fizzbuzz(5, 8, 400) //calling in function name and giving it 3 new arguments
+fizzBuzz(3, 5, 100) //calling the function name and giving it 3 arguments
+fizzbuzz(5, 8, 400) //calling the function name and giving it 3 new arguments
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,41 +1,64 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
-
+var heroName = " ";
+var specialAbility = " ";
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
-
+var greeting = " " + heroName;
+var catchphrase = "We have ${specialAbility}";
 // Declare two variables - power AND energy - set to integers
-
+var power = 1;
+var energy = 2;
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
-
+var fullPower = 1 * 500;
+var fullEnergy = 2 + 150;
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
+var isHuman = true;
+var identityConcealed = false;
 
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
+var archEnemies = ["Enemy1", "Enemy2", "Enemy3"];
+var sidekicks = ["Sidekick1", "Sidekick2", "Sidekick3"];
 
 // Print the first sidekick to your console
-
+console.log(sidekicks[0]);
 // Print the last archEnemy to the console
+console.log(archEnemies[2]);
 
 // Write some code to add a new archEnemy to the archEnemies array
-
+var moreArchEnemies = archEnemies.push("Enemy4");
 // Print the archEnemies array to console to ensure you added a new archEnemy
-
+console.log(archEnemies);
 // Remove the first sidekick from the sidekicks array
-
+varRemoveSidekick = sidekicks.shift()
 // Print the sidekicks array to console to ensure you added a new sidekick
-
+var addSidekick = sidekicks.push("Sidekick4");
+console.log(sidekicks);
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+function assessSituation(dangerLevel, saveTheDay, badExcuse) {
+  var dangerLever = 10;
+  var saveTheDay = "Mission Accomplished";
+  var badExcuse = "This is dangerous";
 
+if (dangerLevel > 50) {
+  console.log("This is dangerous");
+} else if (dangerLevel === 11 && dangerLevel < 50) {
+  console.log("Mission Accomplished");
+} else if (dangerLevel < 10) {
+  console.log("Meh. Hard pass");
+}
+}
+assessSituation(2);
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
@@ -59,13 +82,13 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
 
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
@@ -81,4 +104,3 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 // What parts felt most comfortable to you?
 
 // What skills do you need to continue to practice before starting Mod 1?
-
