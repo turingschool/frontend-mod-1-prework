6f6f7a35a3b91a446cb3d2eeed45d47afbb8b99e
janitastic <palomino.jani@gmail.com>
2021-02-17 01:23:50 -0800

    Add Section 3 work

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,42 +24,50 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = "Gorilla"
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[3] = "Tiger"
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals[4] = "Elephant"
+console.log(animals[4]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = ["Crackers", "Ham", "Cheese", "Nuts"];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods[4] = "broccoli"
+console.log(foods)
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.splice(4)
+console.log(foods)
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+foods.push("gnocchi", "tacos", "fries")
+console.log(foods)
+
 
 // YOU DO: Remove the food that is in index position 0.
+foods.shift()
+console.log(foods)
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,12 +87,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Starbucks - in a drinks array, examples would be hot coffee, iced coffee, hot tea, cold tea
+// 2: Starbucks - in a food array, examples would be sandwich, wrap, egg bites
+// 3: Starbucks - in a coffeeBlend array, examples would be blonde, dark, medium
 
 
 //-------------------
@@ -96,7 +104,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,7 +114,18 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
-
+/* if statement code executes if the percentBatteryLeft variable is greater than the number 14,
+if this is true it returns "can call uber". If this is false it moves on to the next if statement */
+if (percentBatteryLeft > 14) {
+  console.log("can call uber")
+/* if statement code determines if the hasCharger boolean equals true AND the charger type string equals
+"car", then it returns "can call uber". If not it moves on to the next if statement */
+}else if (hasCharger === true && chargerType === "car") {
+  console.log("can call uber")
+/* if none of the above if statements are true, then the return is "can't call uber" */
+}else {
+  console.log("can't call uber")
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
