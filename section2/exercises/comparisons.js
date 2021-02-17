532651d0c4b8d7e9e39a4e69f47eac7abe8c6ec2
janitastic <palomino.jani@gmail.com>
2021-02-16 20:36:25 -0800

    Add Section 2 work

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -23,9 +23,11 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
+console.log ("is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
 // this should log: true
 
 /*
@@ -35,6 +37,7 @@ perform this evaluation
 */
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
+console.log("is numberTeachers strictly equal to stringTeachers", numberTeachers === stringTeachers);
 // this should log: false
 
 /*
@@ -48,18 +51,23 @@ you develop good habits that follow best practice, from now on, use the strict c
 */
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log("is numberTeachers not equal to numberStudents", numberTeachers !== numberStudents);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log("is numberStudents greater than or equal to 20", numberStudents >= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log("is numberStudents greather than or equal to 21", numberStudents >= 21);
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log("is numberStudents less than or equal to 20", numberStudents <= 20);
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log("is numberStudents less than or equal to 2", numberStudents <= 21);
 // this should log: true
 
 
@@ -74,20 +82,35 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 console.log(4 < 9);
 //YOU DO: Explain.
+//log the result of: is 4 less than 9
+//result: true, because the number 4 is less than 9
 
 var books = 3;
-console.logs(4 < books);
+console.log(4 < books);
 // YOU DO: Explain.
+//as written, this would provide an error message, because `console.logs` (plural) is not a prompt.
+//if this is just a typo, then here's what's happening -
+  //the variable of `books` is declared as number of `3`
+  //log the result of: 4 is less than books, or there are more than 4 books
+  //result is: false, because 4 is not less than the declared number of books or 3
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+//the variable of `friends` is declared as a number of `6`
+//the variable of `siblings` is declared as a number of `2`
+//log the results of: there are more friends than siblings
+//result is: true, because the number 6 is greater than the number 2
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
+//the variable is `attendees` is declared as number `9`
+//the variable of `meals` is declared as number `8`
+//log the results of: the amount of attendees does not equal the amount of meals
+//results is: true, 9 does not equal 8
 
 
 //-------------------
@@ -110,22 +133,24 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age <= 1);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
 // ANSWER:
-
+// log the results of dog loves to play and the dogs age is less than or equal to 1 based of my determination that a puppy is less 1 year or younger.
 //-------------------
 // FINAL CHECK
 //-------------------
 
 // Did you run this file in your terminal to make sure everything printed out to the console
 // as you would expect?
+// yes
--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,12 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+};
+
+sayHello();
+sayHello();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -34,18 +40,34 @@ printName();
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
+function greetMe(name) {
+  console.log(name);
+};
 
+greetMe("Jani");
+greetMe("Sarah");
+greetMe("Mike");
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
+function addition(a, b) {
+  console.log(a + b);
+};
 
+addition(3, 4);
+addition(4, 6);
+addition(5, 1);
 
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
+function relationshipStatus(personA, personB) {
+  console.log(personA + " and " + personB + " are BFFS ")
+}
 
+relationshipStatus("Oscar", "Ruck");
 
 
 //-------------------
@@ -60,6 +82,7 @@ Look at the code you wrote for the previous YOU DO🎈 - what did you name the f
 What did you name each parameter, and why?
 
 EXPLAIN:
-
+I named the function relationshipStatus because the end result was "labeling" the two people as best friends.
+I named the two parameters personA and personB because I knew there were two people involved in the function.
 
 */
