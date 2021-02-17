0ddfd1afc6d0c0dea69dc7306ff64901a829b7f7
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-02-16 19:52:14 -0700

    Edit text in fizzBuzz.js

--- finalProject/fizzBuzz.js
+++ finalProject/fizzBuzz.js
@@ -2,13 +2,13 @@
 
 for (var i = 1; i < 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+    console.log("FizzBuzz, ");
   } else if (i % 5 === 0) {
-    console.log("Buzz")
+    console.log("Buzz, ")
    } else if (i % 3 === 0) {
-    console.log("Fizz")
+    console.log("Fizz, ")
   } else {
-   console.log(i);
+   console.log(i + ", ");
   }
 }
 
