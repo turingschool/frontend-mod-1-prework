3b4fb8d0e339fde8a4a3d147440ee469af401dc8
Alex Kio <alexandermartelkio@gmail.com>
2021-02-22 17:53:45 -0500

    Answered concatenation exercises

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -19,33 +19,37 @@ console.log("The " + team + " are " + name + "'s favorite Quidditch team");
 var numberOfCreatures = 7;
 var creatures = "unicorns";
 
-console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
+console.log("My zoo has " + numberOfCreatures + " " + creatures + "!");
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
 // Your answer and explanation here: 
-
+// A string. From the reading, "The reason this works even though the first value is a number and the
+// second is a string, is a result of type coercion. JavaScript is essentially trying to help us by
+// converting the number into a string so we can combine the two together."
 
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
-
+console.log("The " + speedy + " jumped over the " + slowPoke); // The quick red fox jumped over the lazy brown dog
 
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
-
 slowPoke = "tortoise";
 speedy = "hare";
-
+console.log("In a predictable result, the " + slowPoke + " beat the " + speedy + "!"); // In a predictable result, the tortoise beat the hare!
 
 // YOU DO:
 // Declare three variables, name/content/data type of your choice. Think carefully about what 
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
   // Then, log out ONE sentence that incorporates all THREE variables.
-
+var typeOfAnimal = "cat";
+var animalName = "Yoda";
+var yodaAgeYears = 7;
+console.log("I have a " + yodaAgeYears + " year old " + typeOfAnimal + " named " + animalName + "."); // I have a 7 year old cat named Yoda.
 
 //-------------------
 // FINAL CHECK
@@ -53,3 +57,4 @@ speedy = "hare";
 
 // Did you run this file in your terminal to make sure everything printed out to the console 
   // as you would expect? You should be doing this by now, and getting into the habit of it.
+// I did and it is. I am.
