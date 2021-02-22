dbf34a5da23d233c99cd884c9f5226366533ac90
Alex Kio <alexandermartelkio@gmail.com>
2021-02-22 17:13:10 -0500

    Added answers to variable practice

--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -19,33 +19,58 @@ console.log(name);
 // log the result:
 var enrolledStudents = 22;
 console.log(enrolledStudents);
+//FYI this isn't doing what the example description is asking for
+// would that be:
+// var students = 20
+// console.log(students + 2)
 
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
-
+var doNotReturn = 'Harry Potter must not return to Hogwarts!';
+console.log(doNotReturn);
+// do you mean assign when you say save?
 
 // YOU DO:
 // Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
   // Store the appropriate data types in each.
 // log all three variables to the console.
+var firstName;
+var isHungry;
+var numberOfPets;
+firstName = "Harry"; // string
+isHungry = true; // boolean
+numberOfPets = 1; // number
+console.log(firstName, isHungry, numberOfPets); // Harry, true, 1
+//I interpreted the directions to say declare first, then assign values. Hopefully I read it correctly.
 
 // IN WORDS:
 // How did you decide to use the data type you did for each of the three variables above? 
   // Explain.
-
+// I chose to use a string for firstName because a name is text.
+// I chose to use boolean for isHungry because it's a true or false statement.
+// I chose to use a number for numberOfPets because it wants a number as a response.
 
 // YOU DO:
 // Re-assign the values to the three variables from the previous challenge to different 
   // values (but same data type).
 // log all three variables to the console.
-
+var firstName;
+var isHungry;
+var numberOfPets;
+firstName = "Harry"; // string
+isHungry = true; // boolean
+numberOfPets = 1; // number
+firstName = "Hermione"; // string
+isHungry = false; // boolean
+numberOfPets = 5; // number
+console.log(firstName, isHungry, numberOfPets); // Hermione, false, 5
 
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
-
+console.log(healthySnacks + junkFoodSnacks); // 14
 
 //-------------------
 // FINAL CHECK
@@ -53,3 +78,4 @@ var junkFoodSnacks = 8;
 
 // Did you run this file in your terminal to make sure everything printed out to the console 
 // as you would expect? 
+// Answer: I did and it did.
