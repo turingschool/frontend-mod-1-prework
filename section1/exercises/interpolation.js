5922808bb57dd64a0dbf59b225de0eefdc639deb
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-02-25 15:58:12 -0700

    Add Section 1

--- section1/exercises/interpolation.js
+++ section1/exercises/interpolation.js
@@ -22,7 +22,7 @@ var creatures = "unicorns";
 console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here: My zoo has 7 unicorns! This will be logged as the syntax for interpolation is used to inject the values into a string.
 
 
 // YOU DO:
@@ -30,7 +30,7 @@ console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
-
+console.log(`The ${speedy} jumped over the ${slowPoke}`);
 
 // YOU DO:
 // Write code that combines the variables below into a string that
@@ -40,13 +40,16 @@ var slowPoke = "lazy brown dog";
 slowPoke = "tortoise";
 speedy = "hare";
 
-
+console.log(`In a predictable result, the ${slowPoke} beat the ${speedy}!`);
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation. 
+  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
+var beginnerSport = "biking"
+var intermediateSport = "swimming"
+var advancedSport = "running"
 
-
+console.log(`I wanted to compete in a triatholon, which included ${beginnerSport}, ${intermediateSport}, and ${advancedSport}.`)
 //-------------------
 // FINAL CHECK
 //-------------------
