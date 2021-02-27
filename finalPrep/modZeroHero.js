e694b2fc3a079d532ddb5afaf3c088a4f2e27240
Alex Kio <alexandermartelkio@gmail.com>
2021-02-26 21:20:49 -0500

    Initial incomplete merge

--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,45 +1,78 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
+var heroName = "Bat Man";
+var specialAbility = "fricken' lazers";
 
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
+var greeting = "Hello " + heroName;
+var catchphrase = "Eat these ${specialAbility}!";
 
 // Declare two variables - power AND energy - set to integers
+var power = 234;
+var energy = 523;
 
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
+var fullPower = power * 500;
+var fullEnergy = energy + 150;
 
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
-
+var isHuman = true;
+var identityConcealed = false;
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
+var archEnemies = ["The Penguin", "Two Face", "Joker"];
+var sidekicks = ["Cat Woman", "Robin", "Alfred"];
 
 // Print the first sidekick to your console
+console.log(sidekicks[0]);
 
 // Print the last archEnemy to the console
+console.log(archEnemies[2]);
 
 // Write some code to add a new archEnemy to the archEnemies array
+archEnemies.push ("Bane");
 
 // Print the archEnemies array to console to ensure you added a new archEnemy
+console.log(archEnemies);
 
 // Remove the first sidekick from the sidekicks array
+sidekicks.shift();
 
 // Print the sidekicks array to console to ensure you added a new sidekick
+console.log(sidekicks);
 
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
 //   - saveTheDay should be a string a hero would say once they save the day 
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+var dangerLevel = 22;
+var saveTheDay = "PAYME! PAY! ME!";
+var badExcuse = "My britches are soggy... gotta go change.";
+
+function assessSituation(dangerLevel, saveTheDay, badExcuse){
+  if (dangerLevel > 50) {
+    console.log(badExcuse);
+  } else if (dangerLevel < 10){
+    console.log("Meh. Hard pass.") ;
+  } else {
+    console.log(saveTheDay);
+  }
+};
+
+assessSituation(55, saveTheDay, badExcuse);
 
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
 //   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
+
 
 //Test Cases
 var announcement = 'Never fear, the Courageous Curly Bracket is here!';
@@ -55,7 +88,14 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - citiesDestroyed (array)
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
-
+var scaryMonster = {
+  name: "Swamp Thing",
+  smell: "dirt",
+  weight: 688,
+  citiesDestroyed: ["Atlanta", "Decatur"],
+  luckyNumbers: [12, 34, 45, 67, 78],
+  address: {number: 1234, street: "Main Street", state: "Florida", zip: "32601"}
+};
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
@@ -66,6 +106,32 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
 //   - energyLevel = 50 
+class SuperHero {
+  constructor(name, superpower, age) {
+    this.name = name;
+    this.power = superpower;
+    this.age = age;
+    this.archNemesis = "The Syntax Error";
+    this.powerLevel = 100;
+    this.energyLevel = 50;
+  }
+  sayName(name) {
+    console.log(name);
+  }
+  maximizeEnergy() {
+    this.energyLevel = 1000;
+  }
+  gainPower() {
+    this.powerLevel = energyLevel * 10;
+  }
+};
+var batty = new SuperHero("Batman", "fly", 30);
+batty.sayName();
+console.log(batty);
+max.maximizeEnergy();
+console.log(batty);
+gain.gainPower();
+console.log(batty);
 
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
