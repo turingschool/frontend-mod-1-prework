2c014a289a27b0d8c1dab7e8bc39e92a7b483f2d
Eric Sergeant <eric.sergeant.dev@gmail.com>
2021-03-03 11:30:22 -0600

    Complete all exercises in section 4

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,34 +7,53 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+//DONE
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+//DONE
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+//DONE
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+//DONE
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
+//DONE
 // The burrito class should have a method named `addTopping`.
 // This method should accept one argument, a String.
 // The method should add a topping to the list of toppings held in state.
 
+//DONE
 // Call the `addTopping` method on two burritos, then log the burritos to verify
 // that the proteins have been changed.
 
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, toppings) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings;
   }
+  changeProtein (update) {
+    this.protein = update;
+  }
+  addTopping (newTop) {
+    this.toppings.push(newTop);
+  }
+}
+  var veggie = new Burrito ("tofu", "beans and rice", ["cheese", "salsa", "lettuce", "cilantro"]);
+  veggie.addTopping("fajita veggies");
+  console.log(veggie);
 
-  // ADD CODE
-};
+  var chicken = new Burrito ("chicken", "beans and rice", ["cheese", "salsa", "lettuce"]);
+  chicken.changeProtein("fish");
+  console.log(chicken);
 
-// ADD CODE
-
+  var steakFajita = new Burrito ("beef", "fajita veggies", ["cheese", "salsa", "sour cream"]);
+  steakFajita.addTopping("extra cheese");
+  console.log(steakFajita);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -1,13 +1,44 @@
 /*
 In the space below, add to the existing skeleton of a Tweet class.
-
+//DONE
 - A tweet should have a (dynamic) author, content, timeStamp, numberOfLikes, and comments.
+//DONE
 - A tweet should be able to increment the numberOfLikes and add to the list of comments.
-
+//
 Create several instances of your Tweet and log them to the console. Make sure the
 tweet object instances _behave_ as expected.
 */
 
 class Tweet {
+  constructor (author, content, time, likes, comments) {
+    this.author = author;
+    this.content = content;
+    this.timeStamp = time;
+    this.numberOfLikes = likes;
+    this.comments = comments;
+  }
+  addLikes() {
+    this.numberOfLikes += 1;
+  }
+  addComment(response) {
+    this.comments.push(response);
+  }
+}
 
-};
+var firstTweet = new Tweet ("AtrophyLife", "My first ever tweet!", "9:01am", 0, []);
+console.log(firstTweet);
+
+firstTweet.addLikes();
+console.log(firstTweet);
+
+firstTweet.addLikes();
+console.log(firstTweet);
+
+var secondTweet = new Tweet ("AtrophyLife", "My second tweet!", "9:15am", 0, []);
+console.log(secondTweet);
+
+secondTweet.addComment("Your first tweet was better.");
+console.log(secondTweet);
+
+secondTweet.addComment("Welcome to the best free website in the world");
+console.log(secondTweet);
