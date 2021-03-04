bd0d67b6704fd05e90f6b9199aa1dd414df26850
Sydney Bear <sbear3915@gmail.com>
2021-03-04 14:19:36 -0700

    Edit burrito exercise

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -9,25 +9,11 @@ Make sure to run the file with node in your command line.
 
 // The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
-class Burrito {
-  constructor(toppings){
-    this.toppings= toppings;
-    this.protein= "chicken";
-  }
-  changeProtein(){
-    this.protein ="steak";
-    }
-    addToppings(){
-      this.toppings.push("salsa");
-    }
-  }
 
 
 // Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
-var burrito1 = new Burrito (["lettuce","brown rice","tomato"]);
-var burrito2 = new Burrito (["white rice","lettuce",]);
-var burrito3 = new Burrito (["cilantro rice","veggies"]);
+
 
 // The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
@@ -35,29 +21,45 @@ var burrito3 = new Burrito (["cilantro rice","veggies"]);
 
 // Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
-burrito1.changeProtein();
-console.log(burrito1);
+
 
 // The burrito class should have a method named `addTopping`.
 // This method should accept one argument, a String.
 // The method should add a topping to the list of toppings held in state.
-burrito1.addToppings();
-console.log(burrito1)
+
 // Call the `addTopping` method on two burritos, then log the burritos to verify
 // that the proteins have been changed.
-burrito2.addToppings();
-burrito2.changeProtein();
-console.log(burrito2);
-
-burrito3.addToppings();
-burrito3.addToppings();
-console.log(burrito3);
-
-class BurritoInfo {
-  constructor(protein, base) {
+lass BurritoInfo {
+  constructor(protein, base, toppings) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings;
   }
+    changeProtein() {
+      this.protein = "Pork";
+    }
+    addTopping(){
+      this.toppings.push("banana peppers");
+    }
+};
+var chickenBurrito = new BurritoInfo("chicken","white rice",["lettuce","peppers","corn salsa"
+]);
+console.log(chickenBurrito);
+
+var beefBurrito = new BurritoInfo("beef","brown rice", ["tomato","salsa","cheese"]);
+console.log(beefBurrito);
+
+var veggieBurrito = new BurritoInfo("beans","lettuce",["tomato","corn","peppers"]);
+console.log(veggieBurrito);
+
+chickenBurrito.changeProtein();
+console.log(chickenBurrito);
+
+beefBurrito.addTopping();
+console.log(beefBurrito);
+
+chickenBurrito.addTopping();
+console.log(chickenBurrito);
 
   // ADD CODE
 };
