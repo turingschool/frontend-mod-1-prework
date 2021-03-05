1eadcb36dc07ceeecfb7d2e1db5aabddfcdee35b
Shayan Golafshani <shayangolafsh@gmail.com>
2021-03-05 12:24:48 -0700

    Add section 3 work

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,42 +24,57 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = "Gorilla";
+console.log(animals[2]);
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Snow Leopard");
+console.log(animals);
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.push("Elephant");
+console.log(animals[4]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
+var foods = ['Pineapple', 'Peach', 'Pomegranate', 'Papaya']
 
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
+console.log(foods.length);
 
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
 
+foods.push("Brocolli");
+console.log(foods);
+
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop();
+console.log(foods);
 
 
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
 
+foods.unshift("Pedialyte", "Baby Food", "Pure Cane Sugar");
+console.log(foods);
+
 // YOU DO: Remove the food that is in index position 0.
+foods.shift();
+console.log(foods);
 
 //-------------------
 // PART 3: Where are Arrays used?
@@ -79,12 +94,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: var cars = ["Ford Mustanf", "Fiat 500", "Toyota Prius"] //CraigsList
+// 2: var motorcyclesForSaleByOwner = ["Honda CM250", "Yamaha R1", "Suzuki Hayabusa", "Kawasaki Ninja H2"] // also CraigsList
+// 3: var matches = ["Jordan", "Hanaa", "Riku", "Sierra", "Athena"] //matches on Bumble
 
 
 //-------------------
@@ -96,16 +111,30 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
 */
-var percentBatteryLeft = 12;
+var percentBatteryLeft = 14;
 var hasCharger = true;
-var chargerType = "car";
+var chargerType = "wall";
+
+/*
+So this if statement first checks to see if the phone has enough charge,
+if so it short circuit evaluates to true and doesn't care to check if the user
+has the proper type of charger. However, if the percentBatteryLeft is less than 15.
+then it proceeds to check if the user has a charger and that the charger is that of the
+proper type. In this case a car charger.
+*/
 
 // Write your conditional here
+if (percentBatteryLeft >= 15 || hasCharger && chargerType === "car") { //check if you have more than 15% batter or if you have a charger and it's a car charger.
+  console.log("Calling Uber"); //print out that you're calling an uber.
+} else {
+  console.log("Sorry battery level too low. Please charge your phone!"); // if those conditions aren't true print out a message to the user to charge their phone.
+}
+//I suppose you get more nuanced in your messages too.
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
--- section3/reflection.md
+++ section3/reflection.md
@@ -2,14 +2,48 @@
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
 
+I realized that I didn’t know about Setting Specific, Measurable, Achievable, Relevant, and Time-Bound goals helps you to approach learning to code with clarity, focus, and use of appropriate strategies.
+
+Also I’m learning that struggling with something or not understanding it is good, that means that I’m growing.
+
 1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+
+Just the fact that I am trying to learn programming and that I’m giving myself multiple shots at it is demonstrating that I have a growth mindset. In the past, I would have just said I suck at this, it’s not meant for me and I would’ve given up.
+
+I think sometimes I don’t demonstrate a growth mindset when I compare myself to other people who are way ahead of me in life. I forget to look at how far I’ve come. Also, I realize sometimes, I feel bad about myself when I have to think a lot about a problem, I expect myself to automatically come up with a solution, so I’ll try to change that as best as I can.
+
 
 1. Think of a web or mobile application you use regularly. Where is an example of a place that the developers probably use Arrays? Explain.
 
+On CraigsList, developers probably use arrays to list all the vehicles for sale within a certain vicinity.
+You could have a list of all the different cars, or a list of all motorcycles within the vicinity of a certain
+zipcode.
+
 1. In your own words, explain what an Array method is.
+
+An array method, is a method that either returns a value or somehow allows you to manipulate an array.
+I think that they're pretty helpful that they're built in to JavaScript, because otherwise you would have
+to program all those behaviors yourself.
 
 1. What is the purpose of a loop? When we would use a loop in conjunction with array vs. without?
 
+The purpose of a loop is to be able to do a command or run a line of code many times, so usually
+it saves you time because you don't have to type out a command hundreds or even thousands of times.
+
+We could use a loop and array together to print all of the items out of an array. Especially if it's a big array.
+If you don't need to access or print out all the indices of the array then you can just avoid using a for loop.
+
 1. If you had to teach a 5 year old about loops, how would you explain it?
 
+I'd say Johnny, let's make some step to brush your teeth. And ask Johnny to be specific about the steps. If he said
+we need to do it a certain amount of times, I'd ask what action? and then have him right down the amount of time we do
+said action.
+`Action (x times)`
+` thing  you do`
+
+That's kind of the brief model that I'd use, but I think another example could be to use an example like how to take off the tire of a car. Johnny, how many lugnuts are there? He could say 5. And then I would ask, "What specific steps do we have to do to
+remove one lugnut?" I would make him break it down, and then ask, "How many times do we have to do those actions Johnny?"
+
 1. What questions do you still have about Arrays and loops?
+What other tricks are there to access the elements inside an object? I'd like to know more resources to see
+how we can use loops inside of objects.
