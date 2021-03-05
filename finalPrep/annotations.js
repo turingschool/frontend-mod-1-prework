b13048e6b3d440e6fb7085999993c0e14d6d0cc6
Claire Fields <clairefields15@gmail.com>
2021-03-05 16:36:58 -0700

    add final prep work

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -1,11 +1,17 @@
 // Add your annotations, line by line, to the code below using code comments.
 // Use the double // to create a new comment
 
-//Build a Bear
+//Build a Bear. This code uses a function to Build a Bear with a name, age, type
+//of material, clothes and a special power
 function buildABear(name, age, fur, clothes, specialPower) {
+//the bear can introduce itself and say a phrase
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`
+//the bear has a name and an age
   var demographics = [name, age]
+//the bear can tell you about it's unique special power
   var powerSaying = "Did you know that I can " + specialPower + " ?"
+//this is your "bear factory", the name, age, clothes, and 2 out of 3 sayings are
+//dynamic, cost, cuddly factor, and goodnight saying are fixed
   var builtBear = {
     basicInfo: demographics,
     clothes: clothes,
@@ -14,29 +20,45 @@ function buildABear(name, age, fur, clothes, specialPower) {
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
     isCuddly: true,
   }
-
+//ends the function and specificies that builtBear will be called when the function executes
   return builtBear
 }
-
+//two different instances of the bear are built: Fluffy and Sleepy. Fluffy is 4 years old,
+//has brown fur, it wearing pants, jorts and a tanktop. Their special power is giving you nightmares!
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
+//Sleepy is 2 years old, with purple fur, wearing pajamas and a sleeping cap. Their spcial power is sleeping in.
+//The tense of the special power should be changed to "sleep in" so that it matches the powerSaying.
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
 
-//FizzBuzz
+//FizzBuzz. Create a function called FizzBuzz with three parameters
 function fizzBuzz(num1, num2, range) {
+//create a for loop with an initial expression of 0, that runs as long as i is less than or equal
+//to the range, and increment it.
   for (var i = 0; i <= range; i++) {
+//add a conditional else-if to the function. if the remainder of i divided by
+//num1 is exactly equal to 0 AND the remainder of i divided by num2 also is exactly equal to zero
+//print 'fizzbuzz' to the console.
     if (i % num1 === 0 && i % num2 === 0) {
       console.log('fizzbuzz')
+//if the remainder of i divided by num1 is exactly zero, print 'fizz' to the console
     } else if (i % num1 === 0) {
       console.log('fizz')
+//if the remainder of i divided by 2 is exactly zero, print 'buzz' to the console
     } else if (i % num2 === 0) {
       console.log('buzz')
+//if none of these conditions are true, print i to the console.
     } else {
       console.log(i)
     }
   }
 }
 
+//one instance of fizzBuzz is defined with num1=3, num2=5 and range=100
+//this prints 100 lines to the console that are either integers, "fizz", "buzz" or "fizzbuzz"
+//depending on the conditional math above
 fizzBuzz(3, 5, 100)
+//another instance is created with num1=5, num2=5 and range=400. there is an
+//error because buzz is not capitalized
 fizzbuzz(5, 8, 400)
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -1,45 +1,99 @@
 // Challenge - See if you can follow the instructions and complete the exercise in under 30 minutes!
 
 // Declare two variables - heroName AND specialAbility - set to strings
+var heroName = "Bite Lady";
+var specialAbility = "biting";
 
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
+var greeting = "Bonjour, I am " + heroName;
+var catchphrase = `Bim, bam, ${specialAbility}!`;
 
+// console.log(heroName);
+// console.log(specialAbility);
+// console.log(greeting);
+// console.log(catchphrase);
 // Declare two variables - power AND energy - set to integers
+
+var power = 10;
+var energy = 100;
 
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
 
+var fullPower = power * 500;
+var fullEnergy = energy + 150;
+
+// console.log(fullPower);
+// console.log(fullEnergy);
+
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
 
+var isHuman = true;
+var identityConcealed = true;
+
+// console.log(isHuman);
+// console.log(identityConcealed);
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
 
+var archEnemies = ["Steel Nymph", "Kevlar Man", "Invisigirl"]
+var sidekicks = ["Tickle Dragon", "Imposter Monster", "Pinch Baby"]
 // Print the first sidekick to your console
+//console.log(sidekicks[0])
 
 // Print the last archEnemy to the console
+//console.log(archEnemies[2])
 
 // Write some code to add a new archEnemy to the archEnemies array
+//archEnemies.unshift("Cotton Candy Boy")
 
 // Print the archEnemies array to console to ensure you added a new archEnemy
+//console.log(archEnemies)
 
 // Remove the first sidekick from the sidekicks array
+//sidekicks.shift()
 
-// Print the sidekicks array to console to ensure you added a new sidekick
+// Print the sidekicks array to console to ensure you added(removed?) a new sidekick
+//console.log(sidekicks)
 
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+var saveTheDay = "Let that be a lesson to you! Never mess with the teeth!"
+var badExcuse = "Uhh... I need to water my neighbor's plants."
+
+function assessSituation (dangerLevel, saveTheDay, badExcuse){
+  console.log(dangerLevel);
+  console.log(saveTheDay);
+  console.log(badExcuse);
+}
+
+//assessSituation (51, "Let that be a lesson to you! Never mess with the teeth!", "Uhh... I need to water my neighbor's plants.");
 
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
 //   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
+
+function assessSituation (dangerLevel, saveTheDay, badExcuse){
+  if(dangerLevel > 50){
+    console.log(badExcuse)
+  } else if(dangerLevel <10){
+    console.log("Meh. Hard pass.")
+  } else{
+    console.log(saveTheDay)
+  }
+}
+
+// assessSituation(99,saveTheDay,badExcuse);
+// assessSituation (45,saveTheDay,badExcuse);
+// assessSituation (9,saveTheDay,badExcuse);
 
 //Test Cases
 var announcement = 'Never fear, the Courageous Curly Bracket is here!'
@@ -56,29 +110,92 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
 
+var scaryMonster = {
+  name: "BoogaBoo",
+  smell: "caramel",
+  weightInPounds: 300,
+  citiesDestroyed: ["Paris", "Denver", "Bozeman"],
+  luckyNumbers: [1, 12, 24],
+  address: {
+    number: 666,
+    street: "Spoopy Way",
+    state: "New Jersey",
+    zip: 90101,
+  }
+}
+
+//console.log(scaryMonster)
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
+
+// class SuperHero {
+//   constructor(name, superpower, age){
+//     this.name = name;
+//     this.superpower = superpower;
+//     this.age = age;
+//     this.archNemesis = "The Syntax Error";
+//     this.powerLevel = 100;
+//     this.energyLevel = 50;
+//   }
+// }
+
+//var superHero1 = new SuperHero ("Self-Hatred", "Eating your feelings", 30);
+//console.log(superHero1)
 
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
 //   - maximizeEnergy, should update the energyLevel to 1000
 //   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
+class SuperHero {
+  constructor(name, superpower, age){
+    this.name = name;
+    this.superpower = superpower;
+    this.age = age;
+    this.archNemesis = "The Syntax Error";
+    this.powerLevel = 100;
+    this.energyLevel = 50;
+  }
+  sayName(){
+    console.log(this.name)
+  }
+  maximizeEnergy(){
+    this.energyLevel = 1000
+  }
+  gainPower(i){
+    this.powerLevel = this.powerLevel + i
+  }
+}
 
 // - Create 2 instances of your SuperHero class
-
+var superHero2 = new SuperHero ("Octo-hands", "giving you a massage", 25)
+superHero2.sayName();
+superHero2.maximizeEnergy();
+//console.log(superHero2);
+superHero2.gainPower(2);
+//console.log(superHero2);
 
 // Reflection
 // What parts were most difficult about this exerise?
+/*I definitely did not complete the exercise within the 30 minute time limit. I
+had a really hard time retaining/remembering information from the earlier sections, especially
+the proper syntax (I got a kick out of the archNemesis name 😆). When referring to my notes, I
+wish that I had written out a lot more examples of code. I ended up referring to my previous work
+which was helpful. Overall, I need more practice with the concepts,*/
 
 // What parts felt most comfortable to you?
+/* I felt most comfortable with declaring variables, else-if statements, creating
+a class using the constructor function and class methods.
+*/
 
 // What skills do you need to continue to practice before starting Mod 1?
-
+/* I need to practice proper syntax overall, as well as more complex conditional statements
+and functions. I want to get a new notebook and re-write my definitions and keywords to
+include examples of proper use/syntax.  */
