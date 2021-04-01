4335d59746543fc1fc98a439553b19c3f1dd5e6c
Nina Brissey <nbrissey@gmail.com>
2021-04-01 13:12:20 -0700

    Add section 2 work

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -7,7 +7,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 8;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -38,16 +38,22 @@ if (bearChoice === 1) {
 Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
+If the statement in parenthesis (condition - 'doorChoice = 1') evaluates as true then return the string 'hat' otherwise return the string 'scarf'.
 
-2. What variable has a new value assigned to it after the first if statement executes?
+2. What variable has a new value assigned to it after the first if statement executes? bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+'scarf'
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+If the condition bearChoice equals 1 evaluates as true, then print "You offer the bear your 'bearClothing' and the bear shows you a secret passage out!" If it evaluates as false, check to see the next condition, bearChoice equals 2, evalauates as true. If yes, then print "You tell the bear the 'bearClothing' is too small and it starts to cry!" If it evaluates to false, check to see if the next condition, bearChoice equals 3, evaluates to true. If the condition evaluates to true, then print "You run as fast as you can into the next room. It's full of snakes!" Finally, if this condition evaluates to false and none of the previous conditions have been met, then print "You stay with the bear and become it's best friend!"
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+"You run as fast as you can into the next room. It's full of snakes!"
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+"You tell the bear the hat is too small and it starts to cry!"
 
 7. What is your favorite ending?
+"You stay with the bear and become it's best friend!" Of course! Then I'd be like that guy on youtube, who raised that lion and reunited with it when he went back to Africa.
 */
--- section2/exercises/if-statements.js
+++ section2/exercises/if-statements.js
@@ -31,10 +31,10 @@ if (weather == 'sunny') {
   console.log('good to go!');
 }
 
-/* 
+/*
 YOU DO:
-Using the dogAge variable defined below, 
-determine if a dog is a puppy (2 or younger), 
+Using the dogAge variable defined below,
+determine if a dog is a puppy (2 or younger),
 an adult, or elderly (10 or older).
 
 Log to the console the appropriate age range
@@ -43,8 +43,15 @@ Log to the console the appropriate age range
 
 var dogAge = 3;
 // Write your conditional here
-
-
+if (dogAge <= 0) {
+  console.log('This dog is not born yet.')
+} else if (dogAge <= 2) {
+  console.log('puppy');
+} else if (dogAge <= 9) {
+  console.log('adult')
+} else {
+  console.log('elderly')
+}
 /*
 YOU DO:
 Using the numQuarters variable defined below, determine
@@ -65,9 +72,11 @@ When numQuarters = 3, program should log "I have enough money for a gumball"
 var numQuarters = 0;
 
 // Write your conditional here
+if (numQuarters >= 2) {
 console.log("I have enough money for a gumball");
+} else if (numQuarters <= 1) {
 console.log("I don't have enough money for a gumball");
-
+}
 
 /*
 YOU DO:
@@ -85,12 +94,19 @@ When cupsOfFlour = 3 and hasSauce = true, your program should log "I can make pi
 var cupsOfFlour = 1;
 var hasSauce = true;
 
+if (cupsOfFlour >= 2 && hasSauce === true) {
+  console.log("I can make pizza");
+} else {
+  console.log("I cannot make pizza");
+}
 // Write your conditional here
 
 
 /*
-For the last two exercises, an ideal solution probably uses a logical operator. 
+For the last two exercises, an ideal solution probably uses a logical operator.
 Did yours? Do you know what a logical operator in JavaScript is? Google it to answer
 for yourself!
+
+For the last one I did, but I don't understand why I would do it for the one prior to that. I would love an explanation as to why I would use a logical operator for the numQuarters exersize.
 
 */
