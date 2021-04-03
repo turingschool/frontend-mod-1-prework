df12bb0feb920b346aa614511607f9440e83375e
Nina Brissey <nbrissey@gmail.com>
2021-04-02 23:22:00 -0700

    Add section 3 work

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,48 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = 'Gorilla';
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
+animals[3] = 'Lizard';
 
-
-// YOU DO: Write code that will log the String "Elephant" in the animals array
-
+// YOU DO: Write code that will log the "Elephant" in the animals array
+console.log(animals + ' Elephant');
+// or
+animals.unshift('Elephant');
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var healthyFoods = ['squash', 'spinach', 'kale', 'beans'];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(healthyFoods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+healthyFoods.push('broccoli');
+console.log(healthyFoods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+healthyFoods.pop();
+console.log(healthyFoods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
-
+healthyFoods.unshift('berries', 'corn', 'peas');
+console.log(healthyFoods);
 // YOU DO: Remove the food that is in index position 0.
-
+healthyFoods.shift();
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +84,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Spotify - likedSongs
+// 2: Amazon - searchedItems
+// 3: Flo - periodStartDates
 
 
 //-------------------
@@ -96,18 +101,34 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
 */
+//Declare variable percentBatteryLeft
 var percentBatteryLeft = 12;
+//Declare variable hasCharger
 var hasCharger = true;
+//Declare variable chargerType
 var chargerType = "car";
 
 // Write your conditional here
-
-
+//if statement to determine if percentBatteryLeft is greater than or equal to 15
+if (percentBatteryLeft >= 15) {
+//if condition evaluates to true var uberResult equals the value of this string
+  uberResult = 'You have enough battery to call your Uber!';
+//if condition evaluates to false, determine if hasCharger and chargerType both evaluate to true
+} else if (hasCharger === true && chargerType === "car") {
+//if condition evaluates to true var uberResult equals the value of this string
+  uberResult = 'You have a car charger. Plug it in and call your Uber!';
+//if condition evalutes to false...
+} else {
+//uberResult equals the value of this string
+  uberResult = 'Sorry but you are walking home today!';
+}
+//Log the uberResult variable to the console
+console.log(uberResult);
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
 // what the line of code below, does.
