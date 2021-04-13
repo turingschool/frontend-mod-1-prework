d73d912214e4d4702f141cb8bfd884ab4b1bd31a
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-13 14:57:22 -0600

    Add Section 2 work

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -24,10 +24,11 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
 // this should log: true
-
+console.log("Is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
 /*
 Note: this is an example of type coercion. Although stringTeachers is a string and numberStudents is an integer,
 this statement still evaluated to true. JavaScript forces the stringTeachers into an integer to
@@ -36,6 +37,7 @@ perform this evaluation
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
 // this should log: false
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 
 /*
 Note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
@@ -49,18 +51,22 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents greater or equal to 20?", numberStudents >= 20);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20);
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 
 //-------------------
@@ -74,21 +80,25 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 console.log(4 < 9);
 //YOU DO: Explain.
+//Logs the boolean result of the comparison "is 4 less than 9?" which is true
 
 var books = 3;
-console.logs(4 < books);
+console.log(4 < books);
 // YOU DO: Explain.
+// Logs the boolean result of the comparison "is 4 less than the variable books?" book is assigned 3 so the return is false
 
 var friends = 6;
 var siblings = 2;
+//changed console.logs to console.log assuming typo
 console.log(friends > siblings);
 // YOU DO: Explain.
+// logs the boolean comparison of two variables "are friends greater than siblings?" which results in true
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+// Logs the comparison of two variables "Is attendees not equal to meals?" which results in true
 
 //-------------------
 // PART 3: Logical Operators
@@ -110,18 +120,18 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
-
+console.log(lovesToPlay && age < 3);
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: True, both conditions resulted in true which is required to return true for the && operator
 
 //-------------------
 // FINAL CHECK
--- section2/reflection.md
+++ section2/reflection.md
@@ -1,13 +1,21 @@
 ## Section 2 Reflection
 
 1. Regarding the blog posts in Part A, how do you feel about asking questions? Do you tend to ask them too soon, or wait too long, or somewhere in between?
+  - Most of the time it's somewhere in between, sometimes I tend to wait a while before asking however.
 
 1. In this section, we removed some of the supports that you had in Section 1. We didn't give the directions for how to run a file in node, and really sent you off to learn about functions by exploring several outside resources. How did that feel? What was uncomfortable about it? How did it support your learning?
+  - It wasn't uncomfortable for the most part, I did run npm instead of node for the first time and was a little confused then looked it up.
 
 1. What is a conditional statement? Give one example of a daily life conditional. Give one example of where a conditional is probably used in a web application you use.
+  1. Whether or not to make coffee in the morning
+  2. whether or not a user in logged in to a site
 
 1. How do you add multiple conditions to an `if` statement? In your own words, explain how to program reads them and determines what to do.
+  - For adding multiple conditions to an if statement you use `elseif` for each condition you need.
 
 1. What tools are available to you, as a developer, if you want to check that TWO conditions are met? Or, if you want to check that 1 of 2 conditions are met, before running a block of code?
+  - && is true if both conditions are true
+  - || is true if either condition is true
 
 1. What questions do you still have about `if` statements and/or functions?
+  - What advantages does a switch statement have over and if statement?
