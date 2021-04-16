e344803bc766567de5b330cbcad9a92c1afe4e52
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-04-16 11:58:56 -0600

    Add Section 3 exercises and reflection

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,51 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
+console.log(animals.length);
 
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
+console.log(animals[2] = "Gorilla");
 
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+console.log(animals[3] = "Lion");
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+console.log(animals[2]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var food = ["Pizza", "Steak", "Bacon", "Salad"]
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
+console.log(food.length);
 
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+food.unshift("broccoli")
+console.log(food);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+var lessFood = food.pop()
+console.log(food);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+food.push("Orange", "Pho", "Pickles")
 
+console.log(food);
 // YOU DO: Remove the food that is in index position 0.
-
+food.shift()
+console.log(food);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +87,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Twitch game directory (sorted by most viewers)
+// 2: Twitch Streams directory for each game (sorted by most viewers and by user preference reccomendations)
+// 3: Twitch Past Broadcasts (Sorted by date)
 
 
 //-------------------
@@ -96,7 +104,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,6 +114,13 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
+// condition will pass if the user has 15% or more OR has a charger AND it is a car charger
+if(percentBatteryLeft >= 15 || hasCharger && chargerType === "car") {
+  console.log("You can call an Uber!");
+// condition will run if niether side of the OR passes above
+} else {
+  console.log("Charge your phone before calling and Uber");
+}
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
--- section3/reflection.md
+++ section3/reflection.md
@@ -2,14 +2,26 @@
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
 
+  - SMART and ZPD were new to me, but the the things that resonated were "understanding mistakes" and "not being afraid to seek help". "Focusing on the process" is very important to grow any skillset.
+
 1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+
+  - I currently demonstrate focusing on the process and understanding mistakes. However, setting SMART goals are sort of foreign to me.
 
 1. Think of a web or mobile application you use regularly. Where is an example of a place that the developers probably use Arrays? Explain.
 
+  - Twitch might use arrays in displaying directories for streamers, games, and past broadcasts. All of which are sorted either by viewers or dates.
+
 1. In your own words, explain what an Array method is.
+
+  - An array method is something that iterates over or manipulates an array.
 
 1. What is the purpose of a loop? When we would use a loop in conjunction with array vs. without?
 
-1. If you had to teach a 5 year old about loops, how would you explain it?
+  - Loops are used to iterate over an array and produce a result for each item in that array. Generally, loops are used to repeat an action X times.
 
+1. If you had to teach a 5 year old about loops, how would you explain it?
+  - Loops are like a factory built to do a set order of things to what is put in them. Sometimes, something can be put into this factory that it is not built for and thus the factory is shutdown. On the other hand, if the factory is ready it will proceed to do the same actions.
 1. What questions do you still have about Arrays and loops?
+
+ - What would be a good way to avoid infinite loops?
