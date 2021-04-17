d06dd2cf4ebc4d7519852c26273c36f8089eb893
Raquel Hill <raquelturing@gmail.com>
2021-04-17 12:12:38 -0600

    Add Section 1

--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -26,17 +26,17 @@ console.log(enrolledStudents);
 
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
 
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
+// How did you decide to use the data type you did for each of the three variables above?
   // Explain.
 
 
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
 
@@ -51,5 +51,5 @@ var junkFoodSnacks = 8;
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
