979c6368a448cacafb275bf0a65d75d1d7452d04
Christian Smucker <chrissmucker94@gmail.com>
2021-04-18 14:44:17 -0600

    Add section 1

--- section1/exercises/dataTypes.js
+++ section1/exercises/dataTypes.js
@@ -1,6 +1,6 @@
 /*
 In the exercises below, write your own code where indicated
-to achieve the desired result. 
+to achieve the desired result.
 
 Each section has 2 examples that are already completed. Your task is to complete
 any remaining prompt in each section.
@@ -9,23 +9,21 @@ When navigated to the root of this project directory, you should be able to run 
 file from your terminal with the command `node section1/exercises/dataTypes.js`
 */
 
-
 //-------------------
 //STRINGS
 //-------------------
 
 // EXAMPLE: Write code below to log the String "Alan Turing" in the console:
-console.log("Alan Turing");
+console.log('Alan Turing');
 
 // EXAMPLE: Write code below to log `Welcome to Turing!` in the console:
 console.log('Welcome to Turing!');
 
 // YOU DO: Write code below to log `99 bottles of pop on the wall...`:
-
+console.log('99 bottles of soda on the wall...');
 
 // YOU DO: Write code below to log one line from your favorite song or movie:
-
-
+console.log('We should have put locking mechanisms on the vehicle doors.');
 
 //-------------------
 //NUMBERS
@@ -38,36 +36,36 @@ console.log(2 + 2);
 console.log(83 - 7);
 
 // YOU DO: log the result of 6 multiplied by 53 to the console:
-
+console.log(6 * 53);
 
 // YOU DO: log the result of 20 divided by 4 to console:
-
+console.log(20 / 4);
 
 // YOU DO: log the result of the modulo of 10 into 54:
-
-
+console.log(10 % 54);
 
 //-------------------
 // BOOLEANS
 //-------------------
 
 // EXAMPLE: log to the console the result of 1 is equal to 2:
-console.log(1 === 2);
+console.log(2 === 1);
 
 // EXAMPLE: log to the console the result of 7 is greater than 2:
 console.log(7 > 2);
 
 // YOU DO: log to the console the result of "hello" is equal to "Hello":
+console.log('hello' === 'Hello');
 
 // YOU DO: log to the console the result of 3 is not equal to 4:
+console.log(3 != 4);
 
 // YOU DO: log to the console the result of 4 is less than or equal to 5:
-
-
+console.log(4 <= 5);
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
--- section1/relection.md
+++ section1/relection.md
@@ -1,19 +1,20 @@
 ## Section 1 Reflection
 
 1. How did the SuperLearner Article resonate with you? What from this list do you already do? Want to start doing or do more of? Is there anything not on this list, that you would add to it?
-
+- It was interesting to learn about interpolation. I'm going to try to practice more of those exercises.
 2. What are the data types you learned about in this section? In your own words, define each.
-
+- Each we learned during class in mod0. Boolean is true or false, strings are characters surrounded by quotes, integers are numbers, and floats are numbes with decimals.
 3. How would you log the string `"Hello World!"` to the console?
-
+- console.log("Hello World");
 4. What is/are the character(s) you would use to indicate comments in a JavaScript file? What is the purpose of a code comment?
-
+- // for single quotes or /* */ for multi line comments
 5. In your own words, what is a variable? How would you explain it to a 5 year old?
-
+- A variable is a group of characters that are contained in quotes that are basically regular sentences.
 6. Think of a site or app you use frequently. What are three variables that are probably used? Which data type would each of those variables probably hold?
-
+- Google maps. search is a string, distance is a float, and time is a float.
 7. In your own words, explain what concatenation is.
-
+- It is how to include variables into a sentence no matter what the user inputs.
 8. Think of a site or app you use frequently. Where do you think the developers used concatention?
-
+- Google maps. What your home is and your desintation.
 9. What questions do you still have about the work we've done so far? (not having a question is not an option)
+- How does someone know where to implement this code in a project?
