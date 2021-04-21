7696b7ac79397ac4f17e4021e19681af3037a07a
Zach <ztollman@gmail.comm>
2021-04-21 15:30:33 -0600

    finish exercise

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,11 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello () {
+  console.log("Oh, Hello!");
+};
+sayHello();
+sayHello();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -34,18 +39,29 @@ printName();
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
-
+function greetMe (name) {
+  console.log("Yo whats, happening " + name + "!");
+};
+greetMe('John');
+greetMe('Sam');
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
-
-
+function sumNum (Number, Number2){
+  console.log(Number + Number2);
+}
+sumNum(2,4)
+sumNum(4,7)
+sumNum(5,8)
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
-
+function brawlBro(Pog, Pog1){
+  console.log(`${Pog} fights ${Pog1} because he is smelly!`);
+}
+brawlBro("John", "Anthony")
 
 
 //-------------------
@@ -59,7 +75,9 @@ that it is reasonable to type, but descriptive enough that others can infer the 
 Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 What did you name each parameter, and why?
 
-EXPLAIN:
+EXPLAIN:I named it brawlBro because the log was going to print out the two bros fighting! It was also a video game I played when I was younger!
+I named the parameters Pog because I am on twitch often, and I thought it would be easy to remember the bros being called pogs because fights are pog sometimes.
+
 
 
 */
