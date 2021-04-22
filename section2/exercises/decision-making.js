5dfa73c73b7afb84c7c59e6bbdf9c95056f253fe
Joana Brito <joanafiorentini@hotmail.com>
2021-04-22 12:17:18 -0700

    Add Section 2 work

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -39,15 +39,62 @@ Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
 
+In those lines We have two conditional statements : If and else. In the if statement we have an equality
+to be read by the computer from the variable doorChoice and the number 1. if it is true : we've assigned a new value
+to the variable bearClothing and the log will show the new value of the variable in the if statement.
+If it is False: the computer will go to the else statement were we also assigned a new value to the variable
+bearClothing, and the computer will log the new value of the variable in the else statement.
+
 2. What variable has a new value assigned to it after the first if statement executes?
+
+The variable bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
 
+The bearClothing value will be "scarf", because the if statement  gives you only the doorChoice 1
+and the else statement tells you any doorChoice different than 1, will result in the bearClothing = "scarf".
+
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+
+
+I've decided to comment the code here to explain in a bettert way:
+
+if (bearChoice === 1) {
+  console.log("You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!");
+  // This first block of code will execute if the bearChoice variable it is equal to 1, if so action ends here.
+  // if this statement is false , the computer will proceed to the next statement (else if).
+/*
+} else if (bearChoice === 2) {
+  console.log("You tell the bear the " + bearClothing + " is too small and it starts to cry!");
+  // This block of code executes if bearChoice it is equal to 2.If so the action ends here.
+  // If the first statement in the first block of code and this statment are false, the computer will proceed
+  // to the next statement else if .
+
+} else if (bearChoice === 3) {
+  console.log("You run as fast as you can into the next room. It's full of snakes!");
+  // This block of code executes if bearChoice it is equal to 3. If so the action ends here.
+  // If the first two statements and this statment are false, the computer will proceed
+  // to the next statement else .
+
+} else {
+  console.log("You stay with the bear and become it's best friend!");
+  // This block of code will execute if all the statments are false.
+}
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
 
+The end of the stoty will be: You run as fast as you can into the next room. It's full of snakes!
+ It will log the end based on the else if statment code in line 31-32.
+
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
 
+It will define the bearClothing as a "hat" based on the if statment on the code lines 14-15 and
+The end of the story will be log as : You tell the bear the hat is too small and it starts to cry!
+It will log based on the else if statment code lines 29-30.
+
+
 7. What is your favorite ending?
+
+My favorite ending is the bearChoice 1, were you and the bear became friends. I love animals <3
+"You offer the bear your hat and the bear shows you a secret passage out!""
 */
--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,13 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+}
+
+sayHello();
+sayHello();
+
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -34,17 +41,31 @@ printName();
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
-
+function greetMe(name) {
+  console.log("Hello,"+ name + "!");
+}
+greetMe("Joana");
+greetMe("Lucky");
+greetMe("Raquel");
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
-
+function numbers(even, odd) {
+  console.log(even + odd);
+}
+numbers(2, 3);
+numbers(4, 7);
+numbers(8, 9);
 
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
+function closeFriends(humanName, dogName) {
+  console.log(humanName + " and " + dogName +" know each other very well, they are very close friends!");
+}
+closeFriends("Joana", "Lucky");
 
 
 
@@ -59,7 +80,10 @@ that it is reasonable to type, but descriptive enough that others can infer the 
 Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 What did you name each parameter, and why?
 
-EXPLAIN:
+EXPLAIN: I've named the function closeFriends because the string that will log when I call the function
+ is describing how close friends Joana and Lucky are. I've named the parameters as humanName and dogName,
+ because if I want to change the parameters I will need another name of a human and another name of a dog,
+ with this information I'm able to call the function again.
 
 
 */
