c4500fc933644ee5f99c32bd2efa95e9dd9a4577
Samantha Brown <bro7445@gmail.com>
2021-04-23 15:48:46 -0400

    Add Section 4 work

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,17 +7,17 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
 // The burrito class should have a method named `addTopping`.
@@ -28,13 +28,28 @@ Make sure to run the file with node in your command line.
 // that the proteins have been changed.
 
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, toppings, changeProtein, addTopping) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings
   }
+changeProtein() {
+  this.protein = "chicken";
+}
+addTopping(topping) {
+  this.toppings.push("jack fruit");
+}
 
-  // ADD CODE
 };
 
-// ADD CODE
+var burritoOne = new Burrito("chicken", "rice", ["cheedar cheese", "salsa", "salt"]);
+burritoOne.changeProtein();
+console.log(burritoOne);
 
+var burritoTwo = new Burrito("blackBeans", "veggies", ["guacomole", "sour cream", "mango salsa"]);
+burritoTwo.addTopping();
+console.log(burritoTwo);
+
+var burritoThree = new Burrito("Beef", "rice",["bqq sauce", "melted cheese", "pepper"]);
+burritoThree.addTopping();
+console.log(burritoThree);
--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -17,9 +17,39 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+
+}
+
+var bigDog = new Dog();
+console.log(bigDog);
+
+var smallDog = new Dog();
+console.log(bigDog);
+
 // Prompt 2: Snack
 
+class Snack {
+
+}
+
+var salty = new Snack();
+console.log(salty);
+
+var sweet = new Snack();
+console.log(sweet);
+
 // Prompt 3: Shirt
+
+class Shirt {
+
+}
+
+var clean = new Shirt();
+console.log(clean);
+
+var dirty = new Shirt();
+console.log(dirty);
 
 //-------------------
 // Part 2: Properties
@@ -34,9 +64,52 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+constructor() {
+  this.weight = weight;
+  this.breed = breed;
+  this.color = color;
+  }
+}
+
+var myDog = new Dog
+console.log(myDog);
+
+var neighborDog = new Dog();
+console.log(neighborDog);
+
 // Prompt 2: Snack
 
+class Snack {
+constuctor(){
+  this.type = type;
+  this.cost = cost;
+  this.size = size;
+  }
+}
+
+var onTheGo = new Snack();
+console.log(onTheGo);
+
+var atHome = new Snack();
+console.log(atHome);
+
+
 // Prompt 3: Shirt
+
+class Shirt {
+constuctor(){
+  this.type = type;
+  this.size = size;
+  this.color = color;
+  }
+}
+
+var clean = new Shirt();
+console.log(clean);
+
+var dirty = new Shirt();
+console.log(dirty);
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -52,6 +125,49 @@ Make sure to run the file with node in your command line.
 
 // Prompt 1: Dog
 
+class Dog {
+constructor(weight, breed, color) {
+  this.weight = weight;
+  this.breed = breed;
+  this.color = color;
+  }
+}
+
+var myDog = new Dog("53 pounds", "Border Collie", "black");
+console.log(myDog);
+
+var neighborDog = new Dog("30 pounds", "mutt", "tan");
+console.log(neighborDog);
+
 // Prompt 2: Snack
 
+class Snack {
+constructor(type, cost, size) {
+  this.type = type;
+  this.cost = cost;
+  this.size = size;
+  }
+}
+
+var onTheGo = new Snack("Cookie", "6.99", "24 ounces");
+console.log(onTheGo);
+// Question- both of these object instances run but they show differently with the
+// two different ways that I entered the cost. which way is correct?
+var atHome = new Snack("Popcorn", 2.75, "1 pound");
+console.log(atHome);
+
 // Prompt 3: Shirt
+
+class Shirt {
+constructor(type, size, color) {
+  this.type = type;
+  this.size = size;
+  this.color = color;
+  }
+}
+
+var hangingOut = new Shirt("tshirt", "medium", "blue");
+console.log(hangingOut);
+
+var professional = new Shirt("blouse", "small", "pink");
+console.log(professional);
