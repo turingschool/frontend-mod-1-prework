0ae00136b77f441960fdc5ea760998faa711fbde
Samantha Brown <bro7445@gmail.com>
2021-04-24 16:42:21 -0400

    Final Prep work

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -3,40 +3,69 @@
 
 //Build a Bear
 function buildABear(name, age, fur, clothes, specialPower) {
+  //declares buildABear as an method with parameters and opeinging curly braces
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+  //declares variable assigns greeting keyword to value string that includes "name" as interpolater
   var demographics = [name, age];
+  //declares variable assigns demographics keyword to value array
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+  //declares variable assign keyword powerSaying to a string that include concatenation
   var builtBear = {
+    // declares builtBear variable as Object literal
     basicInfo: demographics,
+    // key-value pair string
     clothes: clothes,
+    //key-value pair string
     exterior: fur,
+    //key-value pair string
     cost: 49.99,
+    //key-value pair integer
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
+    //key-value pair array
     isCuddly: true,
+    //key-value pair boolean
   };
+  //closing braces
 
   return builtBear
+  //method
 }
 
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
+//test case
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
-
+//test case
 
 
 //FizzBuzz
 function fizzBuzz(num1, num2, range) {
+  //declares function fizzBuzz with parameters
   for (var i = 0; i <= range; i++) {
+    //creates a loop where i =0 and i is less than "range". loop ends when comes back false
     if (i % num1 === 0 && i % num2 === 0) {
+      //conditional statement
       console.log('fizzbuzz');
+      //what prints if conditional statement is true
     } else if (i % num1 === 0) {
+      //if 1st coditional statement is fasle then this code runs
       console.log('fizz');
+      //if 2nd conditional statement is true, console prints this
     } else if (i % num2 === 0) {
+      //if 2nd coditional statement is fasle then it goes to this code
       console.log('buzz');
+      //if 3rd condition is true then this is what prints "buzz"
     } else {
+      //if all conditions false then this is true
       console.log(i);
+//what prints for everything not covered in first 3 conditonal statments
     }
+    //closing braces
   }
+  //closing braces
 }
+//closing braces for entire function
 
 fizzBuzz(3, 5, 100);
+//test case should print (98, "fizz", "buzz")
 fizzbuzz(5, 8, 400);
+// test case should print (398, 399, "fizzbuzz")
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -2,44 +2,97 @@
 
 // Declare two variables - heroName AND specialAbility - set to strings
 
+var heroName = "AT Legs";
+var specialAbility = "crushing big miles!";
+
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
 
+var greeting = "It's kind of a really nice day" + " says " + heroName;
+var catchPhase = `Living the dream ${specialAbility}`;
+
+console.log(greeting);
+console.log(catchPhase);
+
 // Declare two variables - power AND energy - set to integers
+
+var power = 99
+var energy = 1000
 
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
 
+var fullPower = (power * 500);
+var fullEngery = (energy + 150);
+
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
 
+var isHuman = true;
+var identityConcealed = true;
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
 
+var archEnemies = ["Felix", "Other Legs", "Play By Play" ]
+var sidekicks = ["Daze", "Eleven", "Postcard"]
+
 // Print the first sidekick to your console
+
+console.log(sidekicks[0]);
 
 // Print the last archEnemy to the console
 
+console.log(archEnemies[2]);
+//I think there's a way to do this differently...hmmm remember to look into this
+
 // Write some code to add a new archEnemy to the archEnemies array
+
+archEnemies.push("Patches");
 
 // Print the archEnemies array to console to ensure you added a new archEnemy
 
+console.log(archEnemies);
+
 // Remove the first sidekick from the sidekicks array
+
+sidekicks.shift();
 
 // Print the sidekicks array to console to ensure you added a new sidekick
 
+console.log(sidekicks);
+
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
 
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
 //   - Anything dangerLevel that is between 10 and 50 should result in printing the saveTheDay string to the console
 //   - If the dangerLevel is below 10, it means it is not worth your time and should result in printing the string "Meh. Hard pass." to the console.
+
+var dangerLevel = 90;
+var saveTheDay = "I'm tall and sexy as f-ck!";
+var badExcuse = "It was nice hiking with you";
+
+function assessSituation(dangerLevel, saveTheDay, badExcuse) {
+  if (dangerLevel >= 50) {
+    console.log("It was nice hiking with you");
+} else if
+    (dangerLevel <= 50) && dangerLevel >= 10) {
+    console.log("I'm tall and sexy as f-ck!");
+}  else if
+    (dangerLevel <= 9) {
+    console.log("Meh. Hard pass.");
+  }
+}
+
+assessSituation(90, saveTheDay, badExcuse);
+assessSituation(22, saveTheDay, badExcuse);
+assessSituation(8, saveTheDay, badExcuse);
 
 //Test Cases
 var announcement = 'Never fear, the Courageous Curly Bracket is here!';
@@ -56,16 +109,49 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
 
+var scaryMonster = {
+  name: "NoroMan",
+  smell: "Thru-hiker",
+  weight: "200",
+  citiesDestroyed: ["Neel's Gap", "Hot Springs", "Waynesboro"],
+  luckyNumbers: [1921, 1948, 2017, 2018],
+  address: {
+    number: 4125,
+    street: "Coxey Brown Road",
+    state: "Maryland",
+    zip: 21773
+  }
+
+}
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
+
+class SuperHero {
+  constructor(name, superpower, age) {
+    this.name = name;
+    this.superpower = superpower;
+    this.age = age;
+    this.archNemesis = "The Syntax Error";
+    this.powerLevel = 100;
+    this.energyLevel = 50;
+  }
+  sayName:function() {
+    console.log("name");
+  }
+  maximizeEnergy:function() {
+    console.log(energyLevel === 1000);
+  }
+  gainPower(); {}
+    console.log('n' + powerLevel);
+}
 
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
@@ -74,11 +160,22 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 
 // - Create 2 instances of your SuperHero class
 
+var hero1 = new SuperHero("Pit Boss", "Can eat anything", 37);
+console.log(hero1);
+
+var hero2 = new SuperHero("House Cat", "Napping", 11);
+console.log(hero2);
 
 // Reflection
 // What parts were most difficult about this exerise?
 
+// The hardest parts of this exercise was how slow I went and how hard the class methods section was for me. //
+
 // What parts felt most comfortable to you?
+
+/* I was most comfortable declaring variables and creating objects. Even though putting the object inside of
+an object through me for a loop at first */
 
 // What skills do you need to continue to practice before starting Mod 1?
 
+//I need to continue practicing methods. I also what to continue practicing loops!
