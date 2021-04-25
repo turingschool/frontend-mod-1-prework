9321a8aac6651309f7334ea2eed493be10841c21
Joana Brito <joanafiorentini@hotmail.com>
2021-04-24 17:16:19 -0700

    Add section4 work

--- section4/exercises/classMethods.js
+++ section4/exercises/classMethods.js
@@ -7,17 +7,17 @@ Complete each task, using the Burrito class below as a starting point.
 Make sure to run the file with node in your command line.
 */
 
-// The burrito should also have a property called `toppings`. It should be assigned to 
+// The burrito should also have a property called `toppings`. It should be assigned to
 // the toppings parameter in the constructor.
 
-// Below/outside of the class declaration, create 3 object instances of a burrito. 
+// Below/outside of the class declaration, create 3 object instances of a burrito.
 // The toppings argument should be an Array of Strings.
 
-// The burrito class should have a method named `changeProtein`. 
+// The burrito class should have a method named `changeProtein`.
 // This method should accept one argument, a String.
 // The method should re-assign this.protein to the value that was passed in.
 
-// Call the `changeProtein` method on a burrito, then log the burrito to verify 
+// Call the `changeProtein` method on a burrito, then log the burrito to verify
 // that the protein has been changed.
 
 // The burrito class should have a method named `addTopping`.
@@ -27,14 +27,37 @@ Make sure to run the file with node in your command line.
 // Call the `addTopping` method on two burritos, then log the burritos to verify
 // that the proteins have been changed.
 
+
 class Burrito {
-  constructor(protein, base) {
+  constructor(protein, base, toppings) {
     this.protein = protein;
     this.base = base;
+    this.toppings = toppings;
   }
 
-  // ADD CODE
+  changeProtein() {
+    this.protein = "vegan";
+  }
+
+  addTopping() {
+    this.toppings = this.toppings + " chives";
+  }
+
+  // I founded the answer but i did not understant very well.
 };
 
-// ADD CODE
+var breakfastBurrito = new Burrito('eggs', 'tortilla', ["mossarela cheese", "breakfast sausages","avocado"]);
+console.log(breakfastBurrito);
 
+breakfastBurrito.changeProtein();
+console.log(breakfastBurrito);
+
+var beefBurrito = new Burrito('beef', 'tortilla', ["guacamole", "tomatoes", "black beans", "rice"]);
+console.log(beefBurrito);
+
+beefBurrito.addTopping();
+console.log(beefBurrito);
+
+var chickenBurrito = new Burrito('chicken', 'tortilla', ["guacamole", "salsa verde", "tomatoes","cream cheese"]);
+console.log(chickenBurrito);
+// ADD CODE
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,36 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
+  constructor(name, content, timeStamp, numberOfLikes, comments) {
+    this.author = name;
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = numberOfLikes;
+    this.comments = comments;
+  }
+  addLike() {
+    this.numberOfLikes = this.numberOfLikes + 1;
+  }
+  addComment() {
+      this.comments = this.comments + " I can't belive! I'm finishing 🥳";
+  }
+
 
 };
+
+var joanaTweet = new Tweet("Joana", "I'm about to freak out with those exercises","April 24,2021 at 4:00pm", 100,[" I'm also feeling it", " This week has been crazy! "]);
+console.log(joanaTweet);
+joanaTweet.addComment();
+console.log(joanaTweet);
+
+var carolTweet = new Tweet("Carol", "Today is a happy one! ", "August 5, 2020 at 2:00pm", 3, ["I'm happy for you Carol"]);
+console.log(carolTweet);
+carolTweet.addLike();
+carolTweet.addLike();
+carolTweet.addLike();
+console.log(carolTweet);
+
+var mattTweet = new Tweet("Matt","Feeling the fire","March 5, 2019 at 2:00 pm ", 30, ["Those photos are crazy","The race looks dope", "Finding a way to find you"]);
+console.log(mattTweet);
+mattTweet.addLike();
+console.log(mattTweet);
