a25a1e998fa15942dd98d2c21ff6e6156b7469f3
William Phelps <williamphelps13@gmail.com>
2021-04-29 23:48:54 -0600

    Add updated arrayMethods.md, add all Section 4, add all Final Prep

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,41 +2,65 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+// The function is declared with the function keyword: "function". Next the name of the function is given in camelCase. The parameters are declared which will represent the data we pass into the function via the arguments.
 function buildABear(name, age, fur, clothes, specialPower) {
+// The string data is assigned the variable name of 'greeting'. The first argument listed when we call the function will undergo interpolation and complete the phrase.
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+  // This data is an array and is passed in when we call the function.
   var demographics = [name, age];
+// The parameter specialPower represents the data that is being passed in and in this case undergoes concatenation to form a complete string.
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+//An object is created within the function that allows data to be stored in key-value pairs.
   var builtBear = {
+//This stores a variable declared above in a key to be referenced.
     basicInfo: demographics,
+//Clothes is a parameter and thus can have data passsed in via an argument when the function is called. It is dynamic.
     clothes: clothes,
+//fur is a parameter and thus can have data passsed in via an argument when the function is called.
     exterior: fur,
+//This is a static value and thus does not change unlike the dynamic values above.
     cost: 49.99,
+//An array is stored in a key value pair in which two of the values are dynamic and one is static
     sayings: [greeting, powerSaying, "Goodnight my friend!"],
+//This key value pair is a simple static boolean
     isCuddly: true,
   };
-
+//When the function is called, it will return the builtBear object values
   return builtBear
 }
-
+// We invoke the name of the function and then list the arguments that are passed into the function.
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
+//We passed different arguments into the function then  on the previous invoking of the function.
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
 
 
-
 //FizzBuzz
+//More concise.
+
+//this is a function with 3 parameters (num1, num2, and range)
 function fizzBuzz(num1, num2, range) {
+//Declares a loop. Within the paranthesis is the counter. i is declared as 0. i is less than or equal to the range. i++ is the incrementor.
   for (var i = 0; i <= range; i++) {
+//If i results in an integer and the statement results in true then the following code will execute.
     if (i % num1 === 0 && i % num2 === 0) {
+//code prints fizzbuzz to console
       console.log('fizzbuzz');
+//this is an else if statement giving another option for a true if the above was false.
     } else if (i % num1 === 0) {
+//if this is true then fizz is logged to the console
       console.log('fizz');
+//an additional else if statement gives another chance if the above was false
     } else if (i % num2 === 0) {
+//if this is true then buzz is logged to the console
       console.log('buzz');
+//if all above were false then the else statement automatically runs
     } else {
+//the variable i value is printed to the console
       console.log(i);
     }
   }
 }
-
+//calls the function with the three arguements
 fizzBuzz(3, 5, 100);
+//calls the function with the three different arguements
 fizzbuzz(5, 8, 400);
--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -15,12 +15,37 @@ Make sure to run the file with node in your command line.
   // Log each variable to the console.
   // You do not need any proerties at this time!!
 
+// I COMMENTED OUT MY "PART 1" WHEN I MOVED ON TO "PART 2"
 // Prompt 1: Dog
 
-// Prompt 2: Snack
-
-// Prompt 3: Shirt
-
+// class Dog {
+// }
+//
+// var saraDachshund = new Dog();
+// var theoDachshund = new Dog();
+//
+// console.log(saraDachshund);
+// console.log(theoDachshund);
+//
+// // Prompt 2: Snack
+//
+// class Snack {
+// }
+// var apple = new Snack();
+// var celery = new Snack();
+//
+// console.log(apple);
+// console.log(celery);
+//
+// // Prompt 3: Shirt
+//
+// class Shirt {
+// }
+// var weldwerksShirt = new Shirt();
+// var rockiesShirt = new Shirt();
+//
+// console.log(weldwerksShirt);
+// console.log(rockiesShirt);
 //-------------------
 // Part 2: Properties
 //-------------------
@@ -32,11 +57,44 @@ Make sure to run the file with node in your command line.
   // Create an object instance and store in variable
   // Log that variable to the console.
 
-// Prompt 1: Dog
-
-// Prompt 2: Snack
-
-// Prompt 3: Shirt
+// // Prompt 1: Dog
+//
+// class Dog {
+//   constructor() {
+//     this.name = "Sara";
+//     this.age = 7;
+//     this.coat = "blue and tan";
+//   }
+// }
+// var saraDachshund = new Dog();
+//
+// console.log(saraDachshund);
+//
+// // Prompt 2: Snack
+//
+// class Snack {
+//   constructor() {
+//     this.foodGroup = "fruit";
+//     this.color = "red";
+//     this.healthy = true;
+//   }
+// }
+// var apple = new Snack();
+//
+// console.log(apple);
+//
+// // Prompt 3: Shirt
+//
+// class Shirt {
+//   constructor() {
+//     this.color = "grey";
+//     this.type = "short sleeve";
+//     this.material = "cotton";
+//   }
+// }
+// var weldwerksShirt = new Shirt();
+//
+// console.log(weldwerksShirt);
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -50,8 +108,47 @@ Make sure to run the file with node in your command line.
   // Create 2 object instances and store in variables (passing in different arguments)
   // Log those variables to the console.
 
+//
 // Prompt 1: Dog
+class Dog {
+  constructor(name, age, coat) {
+    this.name = name;
+    this.age = age;
+    this.coat = coat;
+  }
+}
+var saraDachshund = new Dog("Sara", 7, "blue and tan");
+var theoDachshund = new Dog("Theo", 3, "black and tan");
+
+console.log(saraDachshund);
+console.log(theoDachshund);
+
 
 // Prompt 2: Snack
+class Snack {
+  constructor(foodGroup, color, healthy) {
+    this.foodGroup = foodGroup;
+    this.color = color;
+    this.healthy = healthy;
+  }
+}
+var apple = new Snack("fruit", "red", true);
+var celery = new Snack("vegetable", "green", true);
+
+console.log(apple);
+console.log(celery);
 
 // Prompt 3: Shirt
+
+class Shirt {
+  constructor(color, type, material) {
+    this.color = color;
+    this.type = type;
+    this.material = material;
+  }
+}
+var weldwerksShirt = new Shirt("grey", "short sleeve", "cotton");
+var rockiesShirt = new Shirt("blue", "long sleeve", "polyester");
+
+console.log(weldwerksShirt);
+console.log(rockiesShirt);
