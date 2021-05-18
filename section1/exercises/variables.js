5a033866ff593c2e1acfb141aa8a4381c2bb723c
Anna Johnson <ae.johnson2931@gmail.com>
2021-05-18 09:48:40 -0700

    Complete variables.js section

--- section1/exercises/variables.js
+++ section1/exercises/variables.js
@@ -23,33 +23,46 @@ console.log(enrolledStudents);
 // YOU DO:
 // Write code below to save the string 'Harry Potter must not return to Hogwarts!'
 // log that variable to the console.
-
+var hogwarts = ('Harry Potter must not return to Hogwarts!')
+console.log(howarts);
 
 // YOU DO:
-// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`. 
+// Declare three variables, named `firstName`, `isHungry` and `numberOfPets`.
   // Store the appropriate data types in each.
 // log all three variables to the console.
+var firstName = "Anna"
+var isHungry = true
+var numberOfPets = 1
+console.log(firstName);
+console.log(isHungry);
+console.log(numberOfPets);
 
 // IN WORDS:
-// How did you decide to use the data type you did for each of the three variables above? 
-  // Explain.
-
-
+// How did you decide to use the data type you did for each of the three variables above?
+  // Explain. For firstName, I picked a string data types because it was just one thing, my first name.
+// For isHungry, I picked boolean because the variable had the word 'is' which leads to a true or false result.
+//for numberOfPets, I used an integer because it's stating a number of somehting.
 // YOU DO:
-// Re-assign the values to the three variables from the previous challenge to different 
+// Re-assign the values to the three variables from the previous challenge to different
   // values (but same data type).
 // log all three variables to the console.
-
+firstName = "June"
+isHungry = false
+numberOfPets = 0
+console.log(firstName);
+console.log(isHungry);
+console.log(numberOfPets);
 
 // YOU DO:
 // Using the variables below, log the total number of snacks to the console:
 var healthySnacks = 6;
 var junkFoodSnacks = 8;
-
+var totalNumberOfSnacks = healthySnacks + junkFoodSnacks;
+console.log(totalNumberOfSnacks);
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
-// as you would expect? 
+// Did you run this file in your terminal to make sure everything printed out to the console
+// as you would expect?
