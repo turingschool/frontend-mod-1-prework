2c2851c94cd8a79d58d1b2a74271beff5c736bba
Renee Swick-Ziller <reneeswick@gmail.com>
2021-05-23 11:51:31 -0600

    Attempt if-statements

--- section2/exercises/if-statements.js
+++ section2/exercises/if-statements.js
@@ -31,10 +31,10 @@ if (weather == 'sunny') {
   console.log('good to go!');
 }
 
-/* 
+/*
 YOU DO:
-Using the dogAge variable defined below, 
-determine if a dog is a puppy (2 or younger), 
+Using the dogAge variable defined below,
+determine if a dog is a puppy (2 or younger),
 an adult, or elderly (10 or older).
 
 Log to the console the appropriate age range
@@ -43,6 +43,13 @@ Log to the console the appropriate age range
 
 var dogAge = 3;
 // Write your conditional here
+if (dogAge < 3) {
+  console.log("puppy");
+} else if (dogAge === 3,4,5,6,7,8,9) {
+  console.log("adult");
+} else if (dogAge > 10){
+  console.log("elderly");
+}
 
 
 /*
@@ -62,11 +69,15 @@ When numQuarters = 2, program should log "I have enough money for a gumball"
 When numQuarters = 3, program should log "I have enough money for a gumball"
 */
 
-var numQuarters = 0;
+var numQuarters = 3;
 
 // Write your conditional here
+if (numQuarters <= 1) {
+  console.log("I don't have enough money for a gumball");
+}
+else if (numQuarters >= 2) {
 console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
+}
 
 
 /*
@@ -86,10 +97,18 @@ var cupsOfFlour = 1;
 var hasSauce = true;
 
 // Write your conditional here
-
+if (cupsOfFlour = 1 && hasSauce = true) {
+  console.log("I cannot make pizza");
+} if else (cupsOfFlour = 1 && hasSauce = false) {
+  console.log("I cannot make pizza");
+} if else (cupsOfFlour = 2 && hasSauce = true) {
+  console.log("I can make pizza");
+} if else (cupsOfFlour = 3 && hasSauce = true) {
+  console.log("I can make pizza");
+}
 
 /*
-For the last two exercises, an ideal solution probably uses a logical operator. 
+For the last two exercises, an ideal solution probably uses a logical operator.
 Did yours? Do you know what a logical operator in JavaScript is? Google it to answer
 for yourself!
 
