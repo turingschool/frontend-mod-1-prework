388117ad82f93faf19e5124e953079537b53c72e
Anna Johnson <ae.johnson2931@gmail.com>
2021-05-24 21:10:35 -0700

    Add completed arrays files

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,43 +24,49 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
+animals[2] = "Gorilla";
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals[3] = "Lion";
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals[4] = "Elephant";
+console.log(animals[4]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var myFaveFoods = ["Cheeseburgers", "Wine", "Donuts", "Coffee"];
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(myFaveFoods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+myFaveFoods.push("Broccoli");
+console.log(myFaveFoods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+myFaveFoods.pop();
+console.log(myFaveFoods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+myFaveFoods.push("Pizza", "Diet Coke", "Friend Chicken Sandiwch");
+console.log(myFaveFoods);
 
 // YOU DO: Remove the food that is in index position 0.
-
+myFaveFoods.shift();
+console.log(myFaveFoods);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +85,18 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: I feel like I live in my google calendar these days and I know there's got to be lists in there!
+// There's likely lists utilized for the calendar itself, comprised of years, months, weeks, days...
+// on and on down to minutes!
+// 2: Additionally, there's likely lists used for each event that is on my calendar. Each event
+// likely has a list including day & time, duration, locations, attendees, etc.!
+// 3: I could likely go on and on with google calendar but I'll switch it up to one of my
+// heavy hitting apps, the Crumbl Cookie app! There's likely a list containing the weeks
+// flavors -- they change out weekly so I imagine there is a list of standard cookies
+// that the weekly flavors are pulled from.
 
 
 //-------------------
@@ -96,17 +108,29 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
 */
-var percentBatteryLeft = 12;
+var percentBatteryLeft = 15;
 var hasCharger = true;
-var chargerType = "car";
+var chargerType = "wall";
 
 // Write your conditional here
-
+// If the battery percent is greater than or equal to 15%
+if (percentBatteryLeft >= 15) {
+  // the system will communicate to the user that they can call an uber.
+  console.log("You can call an Uber!");
+  // if the user has a charger that is specifically a car charger
+} else if (hasCharger == true && chargerType == "car") {
+  // the system will communicate to the user that they can call an uber.
+  console.log("You can call an Uber!");
+  // if the user does not have at least a 15% battery life OR a car charger,
+  // the system will return that they cannot order an uber.
+} else{
+  console.log("You cannot call an Uber.");
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
