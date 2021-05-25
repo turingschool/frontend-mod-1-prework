1d9c4920df2ba80e48caa8829f018c309a069b25
Natalia Zamboni Vergara <nzamboniv@gmail.com>
2021-05-25 16:57:07 -0600

    Add Section 4 work

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -16,10 +16,31 @@ Make sure to run the file with node in your command line.
   // You do not need any proerties at this time!!
 
 // Prompt 1: Dog
+class Dog {
+};
+
+var dog1 = new Dog();
+var dog2 = new Dog();
+
+console.log(dog1, dog2);
 
 // Prompt 2: Snack
+class Snack {
+};
+
+var snack1 = new Snack();
+var snack2 = new Snack();
+
+console.log(snack1, snack2);
 
 // Prompt 3: Shirt
+class Shirt {
+};
+
+var shirt1 = new Shirt();
+var shirt2 = new Shirt();
+
+console.log(shirt1, shirt2);
 
 //-------------------
 // Part 2: Properties
@@ -33,10 +54,43 @@ Make sure to run the file with node in your command line.
   // Log that variable to the console.
 
 // Prompt 1: Dog
+class DogObject {
+  constructor(){
+  this.breed = "Lab";
+  this.age = 5;
+  this.name = "Sally";
+  }
+};
+
+var dogOne = new DogObject();
+
+console.log(dogOne);
 
 // Prompt 2: Snack
+class SnackObject {
+  constructor(){
+  this.type = "chips";
+  this.expiration = "3/30/2022";
+  this.costDollars = 5;
+  }
+};
+
+var snackOne = new SnackObject();
+
+console.log(snackOne);
 
 // Prompt 3: Shirt
+class ShirtObject {
+  constructor(){
+  this.brand = "H&M";
+  this.material = "100% cotton";
+  this.costDollars = 20;
+  }
+};
+
+var shirtOne = new ShirtObject();
+
+console.log(shirtOne);
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -50,8 +104,42 @@ Make sure to run the file with node in your command line.
   // Create 2 object instances and store in variables (passing in different arguments)
   // Log those variables to the console.
 
-// Prompt 1: Dog
+   // Prompt 1: Dog
+  class DogDynamicObject {
+    constructor(breed, age, name){
+    this.breed = breed;
+    this.age = age;
+    this.name = name;
+    }
+  };
 
-// Prompt 2: Snack
+  var dogDyn = new DogDynamicObject("Pitbull", 2, "Emi");
+
+  console.log(dogDyn);
+
+  // Prompt 2: Snack
+  class SnackDynamicObject {
+    constructor(type, expDate, cost){
+    this.type = type;
+    this.expiration = expDate;
+    this.costDollars = cost;
+    }
+  };
+
+  var snackDyn = new SnackDynamicObject("veggies", "5/30/2021", 10);
+
+  console.log(snackDyn);
+
 
 // Prompt 3: Shirt
+class ShirtDynamicObject {
+  constructor(brand, material, cost){
+  this.brand = brand;
+  this.material = material;
+  this.costDollars = cost;
+  }
+};
+
+var shirtDyn = new ShirtDynamicObject("H&M", "90% cotton", 15);
+
+console.log(shirtDyn);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,34 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
+  constructor(author, content, timeStamp, numberOfLikes, comments){
+    this.author = author;
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = numberOfLikes;
+    this.comments = comments;
+  }
+    addLike(newLike){
+      var countLikes = this.numberOfLikes + newLike;
+      this.numberOfLikes = countLikes;
+    }
+
+    addComment(newComment){
+      this.comments.push(newComment);
+    }
 
 };
+
+var tweet1 = new Tweet("Darin", "Thanks to Mrs. Pacman, I grew up thinking a pretzel was a fruit", "Sep 6, 2017", 5, ["me too", "lol"]);
+console.log(tweet1);
+
+tweet1.addLike(3);
+tweet1.addComment("why though?");
+console.log(tweet1);
+
+var tweet2 = new Tweet("Harle", "i miss when i was little n i thought my heart was heart shaped", "Dec 23, 2020", 357800, ["wait its not ...jk ik i take anatomy", "the day i found out a piece of me died", "i refuse to think otherwise"])
+console.log(tweet2);
+
+tweet2.addLike(1);
+tweet2.addComment("i wish no one told me the truth");
+console.log(tweet2);
