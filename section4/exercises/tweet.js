7f994cda3cbfca01b476ce7012123e523ccc7a2c
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-05-25 21:05:25 -0600

    Add modZeroHero and anotations files and tweet.js last minute modifications

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -7,18 +7,19 @@ tweet object instances _behave_ as expected.
 */
 class Tweet {
 
-   constructor(author, content, timeStamp) {
+   constructor(author, content, timeStamp, comments) {
      this.author = author;
      this.content = content;
      this.timeStamp = timeStamp;
+     this.comments = comments;
 //here with this 3 previous properties we are assigning dynamic values to the the properties of the constructor that is a fabric of objects.
      this.numberOfLikes = 0;
-     this.comments = [];
-//With this 2 previous lines of code we are creating 2 new properties and assigning it a value of 0 to numberOfLikes and an empty array to comments.
+//With this previous lines of code we are creating 1 static property and assigning it a value of 0 to numberOfLikes.
    }
-   addNumberOfLikes(){
-     this.numberOfLikes = ++this.numberOfLikes;
-//With this method we are creating a counter to add the number of likes every time we create and object and call this method.
+   addNumberOfLikes(newLike){
+     var countLikes = this.numberOfLikes + newLike;
+     this.numberOfLikes = countLikes;
+//With this method we are creating a way to add the number of likes to our object by calling this method specifiying in the argument the number of newLikes we want our object to have.
    }
    addComments(newComment) {
      this.comments = this.comments.concat(newComment);
@@ -27,13 +28,13 @@ class Tweet {
 };
 
 var elephants = ['Well said'];// we define a variable with a comment inside of an array.
-var newTweet = new Tweet('Carlos', 'Cool tweet', '05/23/2021 13:00');//we create an object instance called newTweet
+var newTweet = new Tweet('Carlos', 'Cool tweet', '05/23/2021 13:00', 'previous comment 1 ');//we create an object instance called newTweet
 newTweet.addComments(elephants);//we called the method to add the comment to the pre/existent array.
-newTweet.addNumberOfLikes();// And here we call the method to count the number of likes
+newTweet.addNumberOfLikes(4);// And here we call the method to count the number of likes
 console.log(newTweet);// Here we log our new object made by our tweet factory.
 
 var lions = ['Nice comment', 'Another nice comment'];
-var newTweet2 = new Tweet('Carlos', 'Cool tweet2', '05/23/2021 13:10');
+var newTweet2 = new Tweet('Carlos', 'Cool tweet2', '05/23/2021 13:10', 'original comment ');
 newTweet2.addComments(lions);
-newTweet2.addNumberOfLikes();
+newTweet2.addNumberOfLikes(90);
 console.log(newTweet2);
