3dded838876cba4b1a3138b9c295b4d5d27dcf7f
Nolan Caine <nolancaine@gmail.com>
2021-06-06 03:10:25 -0600

    Complete decision-making.js and if-statements.js

--- section2/exercises/decision-making.js
+++ section2/exercises/decision-making.js
@@ -5,9 +5,9 @@ Below is a dynamic story that is created based on the value of three variables: 
 Spend some time changing the variables and running the file to see how the story changes.
 */
 
-var doorChoice = 1;
+var doorChoice = 2;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 0;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -38,16 +38,23 @@ if (bearChoice === 1) {
 Questions
 
 1. In English, using technical vocabulary, describe what is happening between lines 14 and 18.
+This code tests for the condition "doorChoice is strictly equal to 1"; if true, value "hat" is assigned to variable bearClothing, and if false, value "scarf" is assigned.
 
 2. What variable has a new value assigned to it after the first if statement executes?
+bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+"scarf"
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 27 and 35.
+This code tests in succession for a few different cases of strict equality for variable bearChoice. It will return the console.log statement following the first condition to be met in order; or if none are met, return the console.log statment following the else condition.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+Depends what the snakes have in store for you.
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+A sad bear with ill-fitting clothing.
 
 7. What is your favorite ending?
+Definitely the else condition. I want a bear friend.
 */
