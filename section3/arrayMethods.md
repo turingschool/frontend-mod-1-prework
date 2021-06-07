ac019c3402679bc7d8fbec856e54ddf986a0eb76
Nolan Caine <nolancaine@gmail.com>
2021-06-07 01:18:56 -0600

    Create arrayMethods.md and complete arrays.js

---
+++ section3/arrayMethods.md
@@ -0,0 +1,19 @@
+#Assigned Methods to Research
+
+array.pop()
+array.push()
+
+_`arr.push(items)` and `arr.pop()` add and remove, respectively, items from the end of an array._
+
+array.shift()
+array.unshift()
+
+_`arr.unshift(items)` and `arr.shift()` add and remove, respectively, items from the beginning._
+
+#Other Methods I Found Interesting
+
+`array.indexOf(item)` - _returns the index position (always an integer) of a specified element_
+
+`array.splice(index at which to start change, number of elements to remove, item1, ... itemN)` - _removes or replaces elements in an array_
+
+`array.slice(index at which to start copy, index before which to end copy)` - _creates a copy of a portion of an array_
--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -23,44 +23,51 @@ console.log(animals);
 console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
-// animals from above.
-
+console.log(animals.length);
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
-
-
+animals.splice(2, 1, "Gorilla");
+console.log(animals);
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
-
+animals.push("Ferret");
+console.log(animals);
+/*Wasn't sure if position 3 is meant as index 2 or 3...*/
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
-
+animals.splice(2, 1, "Elephant");
+console.log(animals[2]);
 
 //-------------------
 // PART 2: Foods: Array Methods
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
-
+var foods = Array(4);
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.unshift("Broccoli");
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop();
+console.log(foods);
 
-
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+foods.splice(1, 3, "Bell Peppers", "Rice", "Chicken");
+console.log(foods);
 
 // YOU DO: Remove the food that is in index position 0.
-
+foods.shift();
+console.log(foods);
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +86,12 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Followers/friends
+// 2: Photo albums/collections
+// 3: Recommended videos/purchases
 
 
 //-------------------
@@ -96,7 +103,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,7 +113,13 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
-
+if (percentBatteryLeft >= 15) { //Tests whether user's phone has at least 15% battery left
+  console.log("You may call an Uber."); //Tells users with at least 15% battery that they may call an Uber
+} else if  (hasCharger && chargerType === "car") { //Tests whether users with less than 15% battery have a car charger
+  console.log("You may call an Uber."); //Tells user with a car charger that they may call an Uber
+} else { //By now we've narrowed to users with less than 15% battery and no car charger
+  console.log("Sorry, you may not call an Uber."); //Tells users with less than 15% and no car charger that they may not call an Uber
+}
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
 // That comment should describe, in your own words, and as technically precise as possible,
