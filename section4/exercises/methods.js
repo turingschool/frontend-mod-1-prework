9c73820cef8d042ebf26ebd891d012835566c040
Susanna Carey <susannaopal@gmail.com>
2021-06-07 17:34:27 -0400

    Add Section 4

--- section4/exercises/methods.js
+++ section4/exercises/methods.js
@@ -16,5 +16,20 @@ Make sure to run the file with node in your command line.
 var lunchOrder = {
   dish: "BLT",
   beverage: "iced tea",
-  tableNumber: 47
-}
+  tableNumber: 47,
+  prepare: function(){
+    console.log("The order is being worked on.");
+  }
+};
+lunchOrder.prepare();
+
+var lunchOrder = {
+  dish: "BLT",
+  beverage: "iced tea",
+  tableNumber: 47,
+  complete: function(){
+    console.log("The order is complete.");
+
+  }
+};
+lunchOrder.complete();
--- section4/exercises/objects.js
+++ section4/exercises/objects.js
@@ -17,30 +17,45 @@ var foods = {
 };
 
 console.log(foods);
-// Notice that the variable name is foods, plural, because it has the ability to hold info about 
-// potentially many foods. That plural name indicates to another developer that the variable stores 
+// Notice that the variable name is foods, plural, because it has the ability to hold info about
+// potentially many foods. That plural name indicates to another developer that the variable stores
 // either an Array or an Object.
 
 //-------------------
 // Part 1: Zoo Animals
 //-------------------
 
-// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and 
+// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and
 // the number of that type of animal at the zoo.
 
+var zooAnimals = {
+  giraffes: 3,
+  elephants: 5,
+  buffalo: 6,
+};
+
+console.log(zooAnimals);
 
 // Using the zoo that you created above, log the value of the first item in
 // the Object
 
+ console.log(zooAnimals.giraffes);
+
 
 // Add an animal to the zoo Object, and log the updated Object.
 
+zooAnimals.gorilla = 7;
+
+console.log(zooAnimals);
 
 // Change the value for at least one of your animal keys, and log the updated Object.
-
+ zooAnimals.gorillas = 9;
+ console.log(zooAnimals);
 
 // Add another animal to the zoo Object, and log the updated Object.
 
+zooAnimals.peacocks = 11;
+console.log(zooAnimals);
 
 //-------------------
 // Part 2: Email
@@ -53,8 +68,19 @@ It has a sender, a subject, ...
 Declare a variable that stores an Object Literal. Each key should be an attribute of an email and each
 value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+
 Log your email object to the console.
 */
+
+var email = {
+  email: 'gmail',
+  subjectLine: 'Return signed document enclosed',
+  sender: 'Susanna',
+  language: 'English',
+  to: 'Melissa',
+
+}
+console.log(email);
 
 //-------------------
 // Part 3: Many Emails - OPTIONAL EXTENSION
@@ -70,7 +96,7 @@ In the arrays exercise, we gave the following example of an array of Instagram p
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 /*
-Frankly, that was a very simplified version of the Array the Instagram developers have 
+Frankly, that was a very simplified version of the Array the Instagram developers have
 written and work with. Still probably slightly simplified as we don't know what their code
 actually looks like, but it may look more like this:
 */
@@ -82,7 +108,7 @@ var posts = [
     timeStamp: "4:37 PM August 13, 2019",
     numberLikes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -96,12 +122,38 @@ console.log(posts);
 console.log(posts[0]);
 
 /*
-The code snippet above shows an Array with 2 elements. Each element in an 
-Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+The code snippet above shows an Array with 2 elements. Each element in an
+Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 good to have some exposure before going into Mod 1.
 */
 
-// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 // key-value pairs you used in your email Object above.
 // Then, log the email Array to the console.
+
+var emails = [
+{
+    email: 'gmail',
+    subjectLine: 'Return signed document enclosed',
+    sender: 'Susanna',
+    language: 'English',
+    to: 'Melissa',
+},
+{
+    email: 'gmail',
+    subjectLine: 'Copy of letter',
+    sender: 'Susanna',
+    language: 'English',
+    to: 'Holly',
+},
+{
+  email: 'gmail',
+  subjectLine: 'Next weekend',
+  sender: 'Susanna',
+  language: 'English',
+  to: 'Aunt Lucia',
+}
+]
+console.log(emails);
+console.log(emails[0]);
