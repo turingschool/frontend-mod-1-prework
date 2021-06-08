cab83cf0ae78448eaebb3baf7b6d27675264c519
Paul Chambers <paultimothychambers@gmail.com>
2021-06-07 22:14:54 -0600

    Add section 2 work

--- section2/exercises/comparisons.js
+++ section2/exercises/comparisons.js
@@ -24,10 +24,10 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
-
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
 // YOU DO: log the result of the comparison: is numberTeachers equal to stringTeachers? (use the == operator)
 // this should log: true
-
+console.log("Is numberTeachers equal to stringTeachers?", numberTeachers == stringTeachers);
 /*
 Note: this is an example of type coercion. Although stringTeachers is a string and numberStudents is an integer,
 this statement still evaluated to true. JavaScript forces the stringTeachers into an integer to
@@ -36,7 +36,7 @@ perform this evaluation
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers? (use the === operator)
 // this should log: false
-
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers);
 /*
 Note: the strictly equal to operator compares the value of the variable in addition to the type of the variable.
 since the numberTeachers is an integer value and the stringTeachers is a string value, although they are both equal to 4,
@@ -49,18 +49,19 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
-
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20);
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
-
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 21);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
-
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20);
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21);
 
 
 //-------------------
@@ -74,22 +75,47 @@ you develop good habits that follow best practice, from now on, use the strict c
 
 console.log(4 < 9);
 //YOU DO: Explain.
+/*
+This line of code will log "true" to the console. This is because the integer 4 is being compared to the integer 9;
+  specifically, whether or not the integer 4 is of lesser value than the integer 9. Both are number data types, so
+  the comparison is made without having to align both data types first (which will occur when using a comparison
+  operator, as this type of operator will convert any and all non-number data types to a number data type before
+  proceeding with a given comparison).
+*/
 
 var books = 3;
-console.logs(4 < books);
+console.log(4 < books);
 // YOU DO: Explain.
+/*
+This line of code will log "false" to the console. This is because the integer 4 is being compared to the integer 3;
+  specifically, whether or not the integer 4 is of lesser value than the integer 3. The first of the two values being
+  compared is a number and the second is a variable. However, the variable's assigned value is defined as the integer
+  3, so no conversion of data types is neccesary before proceeding with the comparison.
+*/
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+/*
+This line of code will log "true" to the console. This is because the integer 6 is being compared to the integer 2;
+  specifically, whether or not the integer 6 is of greater value than the integer 2. Both values being compared are
+  variables. However, the assigned values of the variables are defined as the integers 6 and 2, respectively, so no
+  conversion of data types is neccesary before proceeding with the comparison.
+*/
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
-
+/*
+This line of code will log "true" to the console. Both data types being compared are variables with numbers as
+  their defined value, those values being the integer 9 and the integer 8, respectively. A strict comparison operator
+  is being used, meaning that no data type conversion will take place before the two variables are compared. In this
+  case, both variables have number data typoe values, so a comparison bewtween these two numbers is able to take place.
+  If the variables were not defined with the same data type, the strict comparison operator would automatically
+  log "false" and move on.
+*/
 //-------------------
 // PART 3: Logical Operators
 //-------------------
@@ -110,19 +136,31 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats);
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+// Two ways to answer this come immediately to mind.
+// First:
+console.log(lovesToPlay && isAPuppy)
+//Second:
+var isAPuppy = (age <= 2)
+console.log(lovesToPlay && isAPuppy)
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
 // ANSWER:
-
+/*
+The first answer evaluates to "undefined", because no value has been defined for that variable name.
+  The second answer evaluates to "true", because the variable "isAPuppy" firt compares the variable "age"
+  with the *given* maximum age (not neccesarily the *true* maximum age) for a dog to still be considered
+  a puppy. This is done in order that a boolean value may be assigned to the variable "isAPuppy" for use
+  by the && operator.
+*/
 //-------------------
 // FINAL CHECK
 //-------------------
--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -26,6 +26,12 @@ printName();
 
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
+function sayHello() {
+  console.log("Oh, Hello!");
+};
+
+sayHello();
+sayHello();
 
 //-------------------
 // PART 2: Arguments and Parameters
@@ -34,18 +40,37 @@ printName();
 // YOU DO: Write a function named greetMe that takes an argument, a String, of a name.
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
+function greetMe(name) {
+  console.log("Hello " + name + ", how are you?");
+};
 
+greetMe("John");
+greetMe("Buggleburg");
+greetMe("MacLandius Scrantonthorn");
 
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
+function sumItUp(num1, num2) {
+  console.log(num1 + num2);
+};
 
+sumItUp(42, 56);
+sumItUp(-398, 42645);
+sumItUp(.004424002640097876, .003099903860002845)
 
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
+function twoStringsWalkIntoABar(name1, name2) {
+//         ^^^^^ Not a good name for a function, I know... :(
+// An alternate name could be something like "twoIntoBar" or "intoBarJoke".
+  console.log(name1 + ' says to ' + name2 + ", 'I don't have a punchline, my bad.'");
+};
+
+twoStringsWalkIntoABar("Barbara", "Gilligan");
 
 
 //-------------------
