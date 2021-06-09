8e3fd59cbb01c761a3cad307381e5b704a23d0b8
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-06-09 12:14:16 -0700

    Complete functions.js

--- section2/exercises/functions.js
+++ section2/exercises/functions.js
@@ -17,7 +17,7 @@ function printName() {
   console.log("Severus Snape");
 };
 
-printName(); 
+printName();
 printName();
 printName();
 printName();
@@ -27,6 +27,13 @@ printName();
 // YOU DO: Write a function named sayHello that logs to the console "Oh, Hello!"
 // Then, call the function 2 times.
 
+function sayHello() {
+  console.log('Oh, Hello!');
+}
+
+sayHello();
+sayHello();
+
 //-------------------
 // PART 2: Arguments and Parameters
 //-------------------
@@ -35,19 +42,34 @@ printName();
 // The function should print out the value of the String that was passed in.
 // Then, call the function 3 times, each time, passing it a different name.
 
+function greetMe(name) {
+  console.log(name);
+}
 
+greetMe('Tom');
+greetMe('Dick');
+greetMe('Harry');
 
 // YOU DO: Write a function that takes in 2 numbers as arguments, Numbers, and logs their sum
 // Then, call that function 3 times, each time, passing in 2 different Numbers.
 
+function sum(num1, num2) {
+  console.log(num1 + num2);
+}
 
+sum(1, 2);
+sum(5, 8);
+sum(9, 3);
 
 // YOU DO🎈: Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be ("Oscar", "Ruck") and
 // the end result might be "Oscar and Ruck are BFFS".  Then, call that function.
 
+function makeSentence(singNoun, directObj) {
+  console.log('The ' + singNoun + ' likes ' + directObj + '.');
+}
 
-
+makeSentence('dog', 'bones');
 //-------------------
 // PART 3: Naming is Hard
 //-------------------
@@ -59,7 +81,5 @@ that it is reasonable to type, but descriptive enough that others can infer the 
 Look at the code you wrote for the previous YOU DO🎈 - what did you name the function, and why?
 What did you name each parameter, and why?
 
-EXPLAIN:
-
-
+EXPLAIN: I named the function makeSentence because by giving it two words (strings), it puts them together to make a sentence. I named the parameters singNoun for 'singular noun' and directObj for 'direct object' to indicate the preferrred types of words for the output to make sense.
 */
