4b2ffee32a9aa223e023879c92a765e8c71dbb28
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-06-10 12:07:09 -0700

    Complete classMethods.js and classProperties.js

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -16,10 +16,34 @@ Make sure to run the file with node in your command line.
   // You do not need any proerties at this time!!
 
 // Prompt 1: Dog
+class Dog {
+
+};
+
+var dog1 = new Dog;
+var dog2 = new Dog;
+
+console.log(dog1, dog2);
 
 // Prompt 2: Snack
+class Shark {
+
+};
+
+var shark1 = new Shark;
+var shark2 = new Shark;
+
+console.log(shark1, shark2);
 
 // Prompt 3: Shirt
+class Shirt {
+
+};
+
+var shirt1 = new Shirt;
+var shirt2 = new Shirt
+
+console.log(shirt1, shirt2);
 
 //-------------------
 // Part 2: Properties
@@ -33,10 +57,40 @@ Make sure to run the file with node in your command line.
   // Log that variable to the console.
 
 // Prompt 1: Dog
+class Dog2 {
+  constructor() {
+    this.breed ='labrador',
+    this.weight = 56,
+    this.isSterilized = true
+  };
+};
+
+var favDog = new Dog2;
+console.log(favDog);
 
 // Prompt 2: Snack
+class Shark2 {
+  constructor() {
+    this.species = 'white shark',
+    this.length = 5,
+    this.eatsHumans = false
+  };
+};
+
+var favShark = new Shark2;
+console.log(favShark);
 
 // Prompt 3: Shirt
+class Shirt2 {
+  constructor() {
+    this.designer = 'Gucci & Gabbana',
+    this.size = 32,
+    this.isStylish = true
+  };
+};
+
+var favShirt = new Shirt2;
+console.log(favShirt);
 
 //-------------------
 // Part 3: Dynamic Properties
