2f5e06ddc68fa0e98f25d968c5c7acf8d50eb1d9
Tony Hsu <tdh425@gmail.com>
2021-06-10 14:16:23 -0600

    Add section4 prework

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -16,10 +16,28 @@ Make sure to run the file with node in your command line.
   // You do not need any proerties at this time!!
 
 // Prompt 1: Dog
+class Dog{}
 
+var dog1 = new Dog();
+console.log(dog1);
+
+var dog2 = new Dog();
+console.log(dog2);
 // Prompt 2: Snack
+class Snack {}
 
+var snack1 = new Snack;
+var snack2 = new Snack;
+console.log(snack1);
+console.log(snack2);
 // Prompt 3: Shirt
+class Shirt {}
+
+var shirt1 = new Shirt;
+var shirt2 = new Shirt;
+
+console.log(shirt1);
+console.log(shirt2);
 
 //-------------------
 // Part 2: Properties
@@ -33,11 +51,36 @@ Make sure to run the file with node in your command line.
   // Log that variable to the console.
 
 // Prompt 1: Dog
+class Dog {
+  constructor() {
+    this.name = 'Kumo';
+    this.age = 1;
+    this.breed = 'Shiba Inu';
+  }
+}
+var dog1 = new Dog();
+console.log(dog1);
 
 // Prompt 2: Snack
-
+class Snack {
+  constructor(){
+    this.name = "Cheetos";
+    this.isSweet = false;
+    this.isMessy = true;
+  }
+}
+var snack1 = new Snack();
+console.log(snack1);
 // Prompt 3: Shirt
-
+class Shirt {
+  constructor() {
+    this.isShortSleeve = true;
+    this.color = 'blue';
+    this.fabric = 'cotton';
+    }
+}
+var shirt1 = new Shirt();
+console.log(shirt1);
 //-------------------
 // Part 3: Dynamic Properties
 //-------------------
@@ -51,7 +94,38 @@ Make sure to run the file with node in your command line.
   // Log those variables to the console.
 
 // Prompt 1: Dog
-
+class Dog {
+  constructor(name, breed, age) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+}
+var dog1 = new Dog('Kumo', 'Shiba Inu', 1);
+console.log(dog1);
+var dog2 = new Dog('Hachi', 'Shepherd mix', 10);
+console.log(dog2);
 // Prompt 2: Snack
-
+class Snack {
+  constructor(name, sweet, calories) {
+    this.name = name;
+    this.sweet = sweet;
+    this.calories = calories;
+  }
+}
+var snack1 = new Snack('Cheetos', false, 200);
+console.log(snack1);
+var snack2 = new Snack('Apple', true, 100);
+console.log(snack2);
 // Prompt 3: Shirt
+class Shirt {
+  constructor(color, size, type) {
+    this.color = color;
+    this.size = size;
+    this.type = type;
+  }
+}
+var shirt1 = new Shirt('red', 'L', 'Short Sleeve');
+console.log(shirt1);
+var shirt2 = new Shirt('grey', 'XL', 'Sweater');
+console.log(shirt2);
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -1,13 +1,33 @@
 /*
 In the space below, add to the existing skeleton of a Tweet class.
-
 - A tweet should have a (dynamic) author, content, timeStamp, numberOfLikes, and comments.
 - A tweet should be able to increment the numberOfLikes and add to the list of comments.
-
 Create several instances of your Tweet and log them to the console. Make sure the
 tweet object instances _behave_ as expected.
 */
-
 class Tweet {
-
+  constructor(author, content, timeStamp, numberOfLikes, comments) {
+    this.author = author;
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = numberOfLikes;
+    this.comments = comments;
+  }
+addComment(comment) {
+  this.numberOfLikes ++;
+  this.comments.push(comment);
+}
 };
+var tweet1 = new Tweet('Sam', 'Going to the beach!', '6/8/21', 5, ['Have fun!', 'Jealous!']);
+console.log(tweet1);
+tweet1.addComment('Can I come?');
+console.log(tweet1);
+tweet1.addComment('Wear sunscreen!');
+console.log(tweet1);
+
+var tweet2 = new Tweet('Alex', 'Anyone want to go to the movies with me?', '6/1/21', 3, ['Im in!', "Sorry can't make it", 'What time?']);
+console.log(tweet2);
+tweet2.addComment('Can I bring my friend?');
+console.log(tweet2);
+tweet2.addComment('Which movie?');
+console.log(tweet2);
