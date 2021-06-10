73f2e6741abb756740932371ed01b9280686bfd3
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-06-10 12:27:06 -0700

    Complete tweet.js

--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,29 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
-
+  constructor(author, content, timeStamp, numberOfLikes, comments) {
+      this.author = author;
+      this.content = content;
+      this.timeStamp = timeStamp;
+      this.numberOfLikes = numberOfLikes;
+      this.comments = comments;
+  };
+  addComment(comment) {
+    this.comments.push(comment);
+  };
+  addLike() {
+    this.numberOfLikes++;
+  };
 };
+
+tweet1 = new Tweet('Bob', 'So, how \'bout them Eagles?', 1203, 4, ['Yeah, they\'re crushin\' it!']);
+console.log(tweet1);
+tweet1.addComment('Can\'t wait till the next game!');
+tweet1.addLike();
+console.log(tweet1);
+
+tweet2 = new Tweet('Sally', '[insert enraged soundbyte about something here]', 1632, 138, []);
+console.log(tweet2);
+tweet2.addComment('I\'m so angry about this too!');
+tweet2.addLike();
+console.log(tweet2);
