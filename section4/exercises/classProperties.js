553838f16690f939671ebfb68cee34b22d1136b7
Paul Chambers <paultimothychambers@gmail.com>
2021-06-10 19:42:39 -0600

    Initial commit and complete exercises

--- section4/exercises/classProperties.js
+++ section4/exercises/classProperties.js
@@ -13,13 +13,40 @@ Make sure to run the file with node in your command line.
   // Write a class skeleton
   // Create 2 object instances and store in variables
   // Log each variable to the console.
-  // You do not need any proerties at this time!!
+  // You do not need any properties at this time!!
 
 // Prompt 1: Dog
+class Dog {
+
+}
+
+var firstNuevoDog = new Dog();
+var secondNuevoDog = new Dog();
+
+console.log(firstNuevoDog);
+console.log(secondNuevoDog);
 
 // Prompt 2: Snack
+class Snack {
+
+}
+
+var firstNuevoSnack = new Snack();
+var secondNuevoSnack = new Snack();
+
+console.log(firstNuevoSnack);
+console.log(secondNuevoSnack);
 
 // Prompt 3: Shirt
+class Shirt {
+
+}
+
+var firstNuevoShirt = new Shirt();
+var secondNuevoShirt = new Shirt();
+
+console.log(firstNuevoShirt);
+console.log(secondNuevoShirt);
 
 //-------------------
 // Part 2: Properties
@@ -33,10 +60,40 @@ Make sure to run the file with node in your command line.
   // Log that variable to the console.
 
 // Prompt 1: Dog
+class Doggie {
+  constructor() {
+    this.breed = "Goldendoodle";
+    this.name = "Colby Jack";
+    this.age = 4;
+  }
+}
+
+var nuevoDoggie = new Doggie();
+console.log(nuevoDoggie);
 
 // Prompt 2: Snack
+class Snackie {
+  constructor() {
+    this.type = "Sugary";
+    this.name = "Ice Cream";
+    this.price = "$1.99";
+  }
+}
+
+var nuevoSnackie = new Snackie();
+console.log(nuevoSnackie);
 
 // Prompt 3: Shirt
+class Shirtie {
+  constructor() {
+    this.style = "Polo";
+    this.brand = "Generic";
+    this.price = "$23.99";
+  }
+}
+
+var nuevoShirtie = new Shirtie();
+console.log(nuevoShirtie);
 
 //-------------------
 // Part 3: Dynamic Properties
@@ -51,7 +108,46 @@ Make sure to run the file with node in your command line.
   // Log those variables to the console.
 
 // Prompt 1: Dog
+class DoggiePoo {
+  constructor(brd, name, age) {
+    this.breed = brd;
+    this.name = name;
+    this.age = age;
+  }
+}
+
+var firstNuevoDoggiePoo = new DoggiePoo("Goldendoodle", "Colby Jack", 4);
+console.log(firstNuevoDoggiePoo);
+
+var secondNuevoDoggiePoo = new DoggiePoo("Bichon Frise", "Neige", 16);
+console.log(secondNuevoDoggiePoo);
 
 // Prompt 2: Snack
+class SnackiePoo {
+  constructor(type, name, moolah) {
+    this.type = type;
+    this.name = name;
+    this.price = moolah;
+  }
+}
+
+var firstNuevoSnackiePoo = new SnackiePoo("Sugary", "Ice Cream", "$1.99");
+console.log(firstNuevoSnackiePoo);
+
+var secondNuevoSnackiePoo = new SnackiePoo("Salty", "Chips", "$2.99");
+console.log(secondNuevoSnackiePoo);
 
 // Prompt 3: Shirt
+class ShirtiePoo {
+  constructor(style, brand, dough) {
+    this.style = style;
+    this.brand = brand;
+    this.price = dough;
+  }
+}
+
+var firstNuevoShirtiePoo = new ShirtiePoo("Polo", "Generic", "$23.99");
+console.log(firstNuevoShirtiePoo);
+
+var secondNuevoShirtiePoo = new ShirtiePoo("T-Shirt", "H&M", "Too Much ($8 billion)");
+console.log(secondNuevoShirtiePoo);
