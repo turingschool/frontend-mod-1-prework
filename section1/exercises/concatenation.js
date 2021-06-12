31a48829796736c727e8290759dd5d17030d3b62
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-06-11 20:48:20 -0700

    Remove unnecessary files and refactor section1

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -1,59 +1,59 @@
 /*
-In the exercises below, write your own code where indicated
-to achieve the desired result.
+In the exercises below, write your own code where indicated to achieve the desired result.
 
-One example is already completed. Your task is to complete
-any remaining prompt.
+One example is already completed. Your task is to complete any remaining prompt.
 
-When navigated to the root of this project directory, you should be able to run this
-file from your terminal with the command `node section1/exercises/dataTypes.js`
+When navigated to the root of this project directory, you should be able to run this file from your terminal with the command `node section1/exercises/dataTypes.js`
 */
 
-// EXAMPLE: Write code that combines the variables below into another string:
-var name = "Ron";
-var team = "Chudley Cannons";
+// EXAMPLE:
+// Write code that combines the variables below into another string:
 
-console.log("The " + team + " are " + name + "'s favorite Quidditch team");
+var name = 'Ron';
+var team = 'Chudley Cannons';
 
-// YOU DO: Read the next few lines of code...
+console.log('The ' + team + ' are ' + name + '\'s favorite Quidditch team');
+
+// YOU DO:
+// Read the next few lines of code...
+
 var numberOfCreatures = 7;
-var creatures = "unicorns";
+var creatures = 'unicorns';
 
-console.log("My zoo has " + numberOfCreatures + " " + creatures + "!");
-
-// YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here:
-// The data type of the console output will be a string due to type coercion, in which JS converts the number to a string so the values can be combined.
+console.log('My zoo has ' + numberOfCreatures + ' ' + creatures + '!');
 
 // YOU DO:
-// Write code that combines the variables below into a string that
-// reads "The quick red fox jumped over the lazy brown dog":
-var speedy = "quick red fox";
-var slowPoke = "lazy brown dog";
-console.log("The " + speedy + " jumped over the " + slowPoke);
+// What data type will be logged to the console as a result of line 22? Explain. Your answer and explanation here:
+
+//The data type of the console output will be a string due to type coercion, in which JS converts the number to a string so that the values can be combined.
 
 // YOU DO:
-// Write code that combines the variables below into a string that
-// reads "In a predictable result, the tortoise beat the hare!"
+// Write code that combines the variables below into a string that reads 'The quick red fox jumped over the lazy brown dog':
+
+var speedy = 'quick red fox';
+var slowPoke = 'lazy brown dog';
+console.log('The ' + speedy + ' jumped over the ' + slowPoke);
+
+// YOU DO:
+// Write code that combines the variables below into a string that reads 'In a predictable result, the tortoise beat the hare!'
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
 
-slowPoke = "tortoise";
-speedy = "hare";
-console.log("In a predictable result, the " + slowPoke + " beat the " + speedy + "!");
+slowPoke = 'tortoise';
+speedy = 'hare';
+console.log('In a predictable result, the ' + slowPoke + ' beat the ' + speedy + '!');
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what
-  // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables.
+// Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!). Then, log out ONE sentence that incorporates all THREE variables.
 
-var car = "mustang";
-var driver = "Michael";
-var style = "skillfully";
-console.log(driver + " drove the " + car + " " + style + "!");
+var car = 'mustang';
+var driver = 'Michael';
+var style = 'skillfully';
+console.log(driver + ' drove the ' + car + ' ' + style + '!');
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console
-  // as you would expect? You should be doing this by now, and getting into the habit of it.
+// Did you run this file in your terminal to make sure everything printed out to the console as you would expect? You should be doing this by now, and getting into the habit of it.
+
+// Yes.
--- section1/exercises/interpolation.js
+++ section1/exercises/interpolation.js
@@ -10,51 +10,51 @@ file from your terminal with the command `node section1/exercises/interpolation.
 */
 
 // EXAMPLE: Write code that combines the variables below into another string:
-var name = "Ron";
-var team = "Chudley Cannons";
+
+var name = 'Ron';
+var team = 'Chudley Cannons';
 
 console.log(`The ${team} are ${name}'s favorite Quidditch team.`);
 
 // YOU DO: Read the next few lines of code...
+
 var numberOfCreatures = 7;
-var creatures = "unicorns";
+var creatures = 'unicorns';
 
 console.log( `My zoo has ${numberOfCreatures} ${creatures}!`);
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
 // Your answer and explanation here:
-// The data type will be a string due to type coercion.
 
+// A string will be logged to the console due to type coercion.
 
 // YOU DO:
-// Write code that combines the variables below into a string that
-// reads "The quick red fox jumped over the lazy brown dog":
-var speedy = "quick red fox";
-var slowPoke = "lazy brown dog";
+// Write code that combines the variables below into a string that reads 'The quick red fox jumped over the lazy brown dog':
+
+var speedy = 'quick red fox';
+var slowPoke = 'lazy brown dog';
 console.log(`The ${speedy} jumped over the ${slowPoke}`);
 
 // YOU DO:
-// Write code that combines the variables below into a string that
-// reads "In a predictable result, the tortoise beat the hare!"
+// Write code that combines the variables below into a string that reads 'In a predictable result, the tortoise beat the hare!'
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
 
-slowPoke = "tortoise";
-speedy = "hare";
+slowPoke = 'tortoise';
+speedy = 'hare';
 console.log(`In a predictable result, the ${slowPoke} beat the ${speedy}!`);
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what
-  // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
-  // Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
+// Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
+// Then, log out ONE sentence that incorporates all THREE variables uisng interpolation.
 
-var fruit = "apples";
-var vegetable = "broccoli";
+var fruit = 'apples';
+var vegetable = 'broccoli';
 console.log(`Eating ${fruit} and ${vegetable} on a daily basis will lead to a long and prosperous life!`)
-
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console
-  // as you would expect? You should be doing this by now, and getting into the habit of it.
+// Did you run this file in your terminal to make sure everything printed out to the console as you would expect? You should be doing this by now, and getting into the habit of it.
+
+// Yes!
