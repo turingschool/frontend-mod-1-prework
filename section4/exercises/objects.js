9889430277fcb23a5e8ae6ef17acac6a81a4a7b8
Kevin Gross <kcgross4@gmail.com>
2021-06-12 02:07:47 -0600

    Syntax Correction on all docs

--- section4/exercises/objects.js
+++ section4/exercises/objects.js
@@ -17,30 +17,38 @@ var foods = {
 };
 
 console.log(foods);
-// Notice that the variable name is foods, plural, because it has the ability to hold info about 
-// potentially many foods. That plural name indicates to another developer that the variable stores 
+// Notice that the variable name is foods, plural, because it has the ability to hold info about
+// potentially many foods. That plural name indicates to another developer that the variable stores
 // either an Array or an Object.
 
 //-------------------
 // Part 1: Zoo Animals
 //-------------------
 
-// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and 
+// YOU DO: Write code below that will declare a variable that stores an Object Literal of animal names and
 // the number of that type of animal at the zoo.
-
+var zooAnimals = {
+  zebras: 4,
+  elephants: 2,
+  lions: 6,
+  penguins: 10
+};
 
 // Using the zoo that you created above, log the value of the first item in
 // the Object
-
+console.log(zooAnimals.zebras);
 
 // Add an animal to the zoo Object, and log the updated Object.
-
+zooAnimals.eagles = 2
+console.log(zooAnimals);
 
 // Change the value for at least one of your animal keys, and log the updated Object.
-
+zooAnimals.lions = 8
+console.log(zooAnimals.lions)
 
 // Add another animal to the zoo Object, and log the updated Object.
-
+zooAnimals.monkeys = 5
+console.log(zooAnimals.monkeys)
 
 //-------------------
 // Part 2: Email
@@ -56,6 +64,14 @@ value should be some appropriate value for that key. Work to have at least 5 key
 Log your email object to the console.
 */
 
+var emails = {
+  date: "06/10/2021",
+  sender: "kcgross4@gmail.com",
+  receiver: "bobjohnson@gmail.com",
+  subject: "Important Notice",
+  body: "Hello, This is an email letting you know that your rent is due on Monday.",
+}
+console.log(emails);
 //-------------------
 // Part 3: Many Emails - OPTIONAL EXTENSION
 //-------------------
@@ -70,7 +86,7 @@ In the arrays exercise, we gave the following example of an array of Instagram p
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 /*
-Frankly, that was a very simplified version of the Array the Instagram developers have 
+Frankly, that was a very simplified version of the Array the Instagram developers have
 written and work with. Still probably slightly simplified as we don't know what their code
 actually looks like, but it may look more like this:
 */
@@ -82,7 +98,7 @@ var posts = [
     timeStamp: "4:37 PM August 13, 2019",
     numberLikes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -96,12 +112,37 @@ console.log(posts);
 console.log(posts[0]);
 
 /*
-The code snippet above shows an Array with 2 elements. Each element in an 
-Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK 
+The code snippet above shows an Array with 2 elements. Each element in an
+Object Literal. Each of those Object Literals has 4 key-value pairs. This may LOOK
 a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 good to have some exposure before going into Mod 1.
 */
 
-// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Object Literals, using the same
 // key-value pairs you used in your email Object above.
 // Then, log the email Array to the console.
+
+var emails = [
+ {
+  date: "03/15/2021",
+  sender: "kcgross4@gmail.com",
+  receiver: "marynguyen@gmail.com",
+  subject: "Happy Birthday!",
+  body: "Happy Birthday Mary! Congrats on being 26 years old!",
+},
+{
+  date: "01/05/2021",
+  sender: "kcgross4@gmail.com",
+  receiver: "taragross@gmail.com",
+  subject: "Vacation Plans",
+  body: "Hi Mom, I was wondering what the plans are for Florida this summer.",
+},
+{
+  date: "10/20/2020",
+  sender: "brandonc@gmail.com",
+  receiver: "kcgross4@gmail.com",
+  subject: "Job Offer",
+  body: "Hello Kevin, we at Google would LOVE to hire you onto our software development team! Please respond ASAP!",
+}
+]
+console.log(emails)
--- section4/exercises/tweet.js
+++ section4/exercises/tweet.js
@@ -9,5 +9,17 @@ tweet object instances _behave_ as expected.
 */
 
 class Tweet {
-
+  constructor(author, content, timeStamp, numberOfLikes, comments) {
+    this.author = author;
+    this.content = content;
+    this.timeStamp = timeStamp;
+    this.numberOfLikes = numberOfLikes;
+    this.comments = comments;
+  }
 };
+var tweet1 = new Tweet("Kevin", "Sports", "12:00", 109, 14);
+console.log(tweet1);
+tweet1 = new Tweet("Kevin", "Sports", "12:00", 230, 25);
+console.log(tweet1);
+tweet1 = new Tweet("Kevin", "Sports", "12:00", 1000, 76);
+console.log(tweet1);
